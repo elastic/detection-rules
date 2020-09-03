4e1e3d12d8fd91346214604c973c4ba73fedf0cd
Stijn Holzhauer <stijnholzhauer+git@gmail.com>
2020-09-03 13:36:08 +0200

    Removing duplicate whiteline

--- detection_rules/eswrap.py
+++ detection_rules/eswrap.py
@@ -244,7 +244,6 @@ def kibana_upload(toml_files, kibana_url, cloud_id, user, password, space):
     if space:
         kibana_url = "{}/s/{}".format(kibana_url, space)
 
-
     with Kibana(cloud_id=cloud_id, url=kibana_url) as kibana:
         kibana.login(user, password)
 
