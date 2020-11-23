e7ea33591275171dff1396e747838ba068e9fcac
SHolzhauer <stijnholzhauer+git@gmail.com>
2020-11-23 18:11:37 +0100

    Adding --preserve-id option and merging upstream

--- detection_rules/kbwrap.py
+++ detection_rules/kbwrap.py
@@ -4,20 +4,31 @@
 
 """Kibana cli commands."""
 import click
-from kibana import Kibana, RuleResource
+import kql
+from kibana import Kibana, Signal, RuleResource
 
 from .main import root
-from .misc import client_error, getdefault
+from .misc import add_params, client_error, kibana_options
 from .rule_loader import load_rule_files, load_rules
 from .utils import format_command_options
 
 
+def get_kibana_client(cloud_id, kibana_url, kibana_user, kibana_password, **kwargs):
+    """Get an authenticated Kibana client."""
+    if not (cloud_id or kibana_url):
+        client_error("Missing required --cloud-id or --kibana-url")
+
+    # don't prompt for these until there's a cloud id or Kibana URL
+    kibana_user = kibana_user or click.prompt("kibana_user")
+    kibana_password = kibana_password or click.prompt("kibana_password", hide_input=True)
+
+    with Kibana(cloud_id=cloud_id, kibana_url=kibana_url, **kwargs) as kibana:
+        kibana.login(kibana_user, kibana_password)
+        return kibana
+
+
 @root.group('kibana')
-@click.option('--kibana-url', '-k', default=getdefault('kibana_url'))
-@click.option('--cloud-id', default=getdefault('cloud_id'))
-@click.option('--kibana-user', '-u', default=getdefault('kibana_user'))
-@click.option('--kibana-password', '-p', default=getdefault('kibana_password'))
-@click.option('--space', default=None)
+@add_params(*kibana_options)
 @click.pass_context
 def kibana_group(ctx: click.Context, **kibana_kwargs):
     """Commands for integrating with Kibana."""
@@ -29,22 +40,14 @@ def kibana_group(ctx: click.Context, **kibana_kwargs):
         click.echo(format_command_options(ctx))
 
     else:
-        if not kibana_kwargs['cloud_id'] or kibana_kwargs['kibana_url']:
-            client_error("Missing required --cloud-id or --kibana-url")
-
-        # don't prompt for these until there's a cloud id or Kibana URL
-        kibana_user = kibana_kwargs.pop('kibana_user', None) or click.prompt("kibana_user")
-        kibana_password = kibana_kwargs.pop('kibana_password', None) or click.prompt("kibana_password", hide_input=True)
-
-        with Kibana(**kibana_kwargs) as kibana:
-            kibana.login(kibana_user, kibana_password)
-            ctx.obj['kibana'] = kibana
+        ctx.obj['kibana'] = get_kibana_client(**kibana_kwargs)
 
 
 @kibana_group.command("upload-rule")
+@click.option('--preserve-id', is_flag=True, help='Preserve the rule_id from toml file as rule id')
 @click.argument("toml-files", nargs=-1, required=True)
 @click.pass_context
-def upload_rule(ctx, toml_files):
+def upload_rule(ctx, preserve_id, toml_files):
     """Upload a list of rule .toml files to Kibana."""
     from uuid import uuid4
     from .packaging import manage_versions
@@ -65,10 +68,42 @@ def upload_rule(ctx, toml_files):
         payload = rule.contents.copy()
         meta = payload.setdefault("meta", {})
         meta["original"] = dict(id=rule.id, **rule.metadata)
-        payload["rule_id"] = str(uuid4())
+        if preserve_id:
+            print(rule.id)
+            payload["rule_id"] = rule.id
+        else:
+            payload["rule_id"] = str(uuid4())
         payload = downgrade(payload, kibana.version)
         rule = RuleResource(payload)
         api_payloads.append(rule)
 
-    rules = RuleResource.bulk_create(api_payloads)
-    click.echo(f"Successfully uploaded {len(rules)} rules")
+    with kibana:
+        rules = RuleResource.bulk_create(api_payloads)
+        click.echo(f"Successfully uploaded {len(rules)} rules")
+
+
+@kibana_group.command('search-alerts')
+@click.argument('query', required=False)
+@click.option('--date-range', '-d', type=(str, str), default=('now-7d', 'now'), help='Date range to scope search')
+@click.option('--columns', '-c', multiple=True, help='Columns to display in table')
+@click.option('--extend', '-e', is_flag=True, help='If columns are specified, extend the original columns')
+@click.pass_context
+def search_alerts(ctx, query, date_range, columns, extend):
+    """Search detection engine alerts with KQL."""
+    from eql.table import Table
+    from .eswrap import MATCH_ALL, add_range_to_dsl
+
+    kibana = ctx.obj['kibana']
+    start_time, end_time = date_range
+    kql_query = kql.to_dsl(query) if query else MATCH_ALL
+    add_range_to_dsl(kql_query['bool'].setdefault('filter', []), start_time, end_time)
+
+    with kibana:
+        alerts = [a['_source'] for a in Signal.search({'query': kql_query})['hits']['hits']]
+
+    table_columns = ['host.hostname', 'signal.rule.name', 'signal.status', 'signal.original_time']
+    if columns:
+        columns = list(columns)
+        table_columns = table_columns + columns if extend else columns
+    click.echo(Table.from_list(table_columns, alerts))
+    return alerts
