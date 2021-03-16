8e0205509c4bc812e080fbe6ef6a73f22a14459a
Ross Wolf <31489089+rw-access@users.noreply.github.com>
2021-03-16 15:57:14 -0600

    Change asset type for integration to security-rule

--- detection_rules/packaging.py
+++ detection_rules/packaging.py
@@ -481,7 +481,7 @@ class Package(object):
 
         package_dir = Path(save_dir).joinpath(manifest.version)
         docs_dir = package_dir / 'docs'
-        rules_dir = package_dir / 'kibana' / 'rules'
+        rules_dir = package_dir / 'kibana' / 'security-rule'
 
         docs_dir.mkdir(parents=True)
         rules_dir.mkdir(parents=True)
