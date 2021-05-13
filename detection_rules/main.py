81bca3e01a3729f5e950aee5924e79c4065531b0
Brent Murphy <bmurphy@endgame.com>
2021-05-13 09:25:02 -0400

    update help prompt

--- detection_rules/main.py
+++ detection_rules/main.py
@@ -113,8 +113,8 @@ def import_rules(input_file, directory):
 
 
 @root.command('toml-lint')
-@click.option('--rule-file', '-f', multiple=True, type=click.Path('r'),
-              help='Optionally specify a specific rule file only')
+@click.option('--rule-file', '-f', multiple=True, type=click.Path(exists=True),
+              help='Specify one or more rule files.')
 def toml_lint(rule_file):
     """Cleanup files with some simple toml formatting."""
     if rule_file:
