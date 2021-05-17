971667dc43952c995379fd0439d1176f5c1eb5d4
Ross Wolf <31489089+rw-access@users.noreply.github.com>
2021-05-17 13:56:04 -0600

    Make a hidden token prompt

--- detection_rules/devtools.py
+++ detection_rules/devtools.py
@@ -232,7 +232,7 @@ def kibana_commit(ctx, local_repo: str, github_repo: str, ssh: bool, kibana_dire
 
 
 @dev_group.command("kibana-pr")
-@click.option("--token", required=True, default=get_github_token(), help="GitHub token to use for the PR")
+@click.option("--token", required=True, prompt=True, default=get_github_token(), help="GitHub token to use for the PR", hide_input=True)
 @click.option("--assign", multiple=True, help="GitHub users to assign the PR")
 @click.option("--label", multiple=True, help="GitHub labels to add to the PR")
 # Pending an official GitHub API
