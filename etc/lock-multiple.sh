6549be286a1358058b3818bad2c92dd24a8080ca
Ross Wolf <31489089+rw-access@users.noreply.github.com>
2021-08-25 14:15:39 -0600

    Fix script

--- etc/lock-multiple.sh
+++ etc/lock-multiple.sh
@@ -5,12 +5,12 @@ set -e
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # switch to
-for branch in $(echo $@ | sed "s/,/ /g")
+for BRANCH in $(echo $@ | sed "s/,/ /g")
 do
-    git checkout $branch
+    echo $BRANCH
+    git checkout $BRANCH
     git pull
     python -m detection_rules dev build-release --update-version-lock
-    echo $branch
 done
 
-git checkout $(CURRENT_BRANCH)
+git checkout ${CURRENT_BRANCH}
