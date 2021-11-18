31eebc8fc5458e634469c4da534f4a2726e34b9e
Domenico Andreoli <domenico.andreoli@elastic.co>
2021-11-18 10:58:13 +0100

    centralized adding of rule name and timestamp

--- detection_rules/events_emitter.py
+++ detection_rules/events_emitter.py
@@ -5,20 +5,26 @@
 
 """Functions for generating event documents that would trigger a given rule."""
 
+import time
 from typing import List
 
 from .rule import AnyRuleData
 
-def _generate_error_docs(rule: AnyRuleData, message: str) -> List[str]:
-    return [{"rule.name": rule.name, "error.message": message}]
+def _generate_error_docs(message: str) -> List[str]:
+    return [{"error.message": message}]
 
 def emit_events(rule: AnyRuleData) -> List[str]:
     if rule.type not in ("query", "eql"):
-        docs = _generate_error_docs(rule, f"Unsupported rule type: {rule.type}")
+        docs = _generate_error_docs(f"Unsupported rule type: {rule.type}")
     elif rule.language == "eql":
         from .events_emitter_eql import emit_events_eql
         docs = emit_events_eql(rule.validator.ast)
     else:
-        docs = _generate_error_docs(rule, f"Unsupported rule language: {rule.language}")
+        docs = _generate_error_docs(f"Unsupported query language: {rule.language}")
 
+    for doc in docs:
+        doc.update({
+            "@timestamp": int(time.time() * 1000),
+            "rule.name": rule.name,
+        })
     return docs
