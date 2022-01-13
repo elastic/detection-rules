b4938a760aeb718a58af89c5208958ef9a26eca4
brokensound77 <brokensound77@users.noreply.github.com>
2022-01-12 16:13:57 -0900

    Prepare for creation of 8.1 branch

--- detection_rules/schemas/__init__.py
+++ detection_rules/schemas/__init__.py
@@ -178,6 +178,12 @@ def migrate_to_7_16(version: Version, api_contents: dict) -> dict:
     return strip_additional_properties(version, api_contents)
 
 
+@migrate("8.0")
+def migrate_to_8_0(version: Version, api_contents: dict) -> dict:
+    """Default migration for 8.0."""
+    return strip_additional_properties(version, api_contents)
+
+
 def downgrade(api_contents: dict, target_version: str, current_version: Optional[str] = None) -> dict:
     """Downgrade a rule to a target stack version."""
     from ..packaging import current_stack_version
