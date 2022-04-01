648daf12373f9e9cfefe5a594ebdedc4bc93c72a
Terrance DeJesus <99630311+terrancedejesus@users.noreply.github.com>
2022-04-01 17:33:18 -0400

    Prep for Creation of 8.3 Branch (#1906)
    * updating with changes for 8.3 prep
    * adding updates
    * adjusted version in packages.yml

--- detection_rules/schemas/__init__.py
+++ detection_rules/schemas/__init__.py
@@ -190,6 +190,12 @@ def migrate_to_8_1(version: Version, api_contents: dict) -> dict:
     return strip_additional_properties(version, api_contents)
 
 
+@migrate("8.2")
+def migrate_to_8_2(version: Version, api_contents: dict) -> dict:
+    """Default migration for 8.2."""
+    return strip_additional_properties(version, api_contents)
+
+
 def downgrade(api_contents: dict, target_version: str, current_version: Optional[str] = None) -> dict:
     """Downgrade a rule to a target stack version."""
     from ..packaging import current_stack_version
