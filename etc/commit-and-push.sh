ed3c8fe8666f8d1ea8554439d0be4d975b690d20
Mika Ayenson <mika.ayenson@elastic.co>
2022-04-01 10:49:23 -0400

    wip: break out the logic to a script and manual workflow

---
+++ etc/commit-and-push.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -x
+set -e
+
+echo "Switch to the target branch and keep the staged changes"
+TARGET_BRANCH=$1
+COMMIT_SHA=$2
+echo "Backporting from commit ${COMMIT_SHA} on branch ${TARGET_BRANCH}"
+git checkout ${TARGET_BRANCH}
+
+NEEDS_BACKPORT=$(git diff HEAD --quiet --exit-code && echo n || echo y)
+
+if [ "n" = "$NEEDS_BACKPORT" ]
+then
+echo "No changes to backport"
+exit 0
+fi
+
+echo "Create the new commit with the same author"
+git commit --reuse-message ${TARGET_BRANCH}
+
+echo "Save the commit message"
+git log ${TARGET_BRANCH} --format=%B -n1 > $COMMIT_MSG_FILE
+
+echo "Append to the commit message"
+if [ -s "$UNSTAGED_LIST_FILE" ]
+then
+echo "Track note for the removed files"
+
+echo "" >> $COMMIT_MSG_FILE
+echo "Removed changes from:" >> $COMMIT_MSG_FILE
+awk '{print "- " $0}' $UNSTAGED_LIST_FILE >> $COMMIT_MSG_FILE
+echo "" >> $COMMIT_MSG_FILE
+echo '(selectively cherry picked from commit ${TARGET_BRANCH})' >> $COMMIT_MSG_FILE
+else
+echo "No removed files"
+
+echo "" >> $COMMIT_MSG_FILE
+echo '(cherry picked from commit ${TARGET_BRANCH})' >> $COMMIT_MSG_FILE
+fi
+
+echo "Amend the commit message and push"
+git commit --amend -F $COMMIT_MSG_FILE
+git push
