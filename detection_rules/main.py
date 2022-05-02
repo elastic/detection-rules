elastic/detection-rules

commit 6ed1a39efeb444222694b0a8e92d72f8d77bd6b0

Add a RuleCollection object instead of a "loader" module (#1063)
* Add a RuleCollection object instead of a "loader" module
* Remove legacy loader code
* Remove more legacy loader
* Freeze the default collection
* Change RULE_LOADER default
* Rename to _toml_load_cache
* Use rglob magic
* Typo should've been a string
* Remove no longer needed glob import
* Fix pycharm import bad ordering
* Restore the detection_rules/schemas imports
* Put more imports back for a smaller diff
* Check cache in _deserialize_toml
* Add multi collection and single collection decorators
* Reorder RuleCollection methods
* Move filter method up

detection_rules/main.py
@@ -4,6 +4,7 @@
 # 2.0.
 
 """CLI commands for detection_rules."""
+import dataclasses
 import glob
 import json
 import os
@@ -11,18 +12,18 @@ import re
 import time
 from pathlib import Path
 from typing import Dict
+from uuid import uuid4
 
 import click
-import jsonschema
 
 from . import rule_loader
-from .cli_utils import rule_prompt
-from .misc import client_error, nested_set, parse_config
+from .cli_utils import rule_prompt, multi_collection
+from .misc import nested_set, parse_config
 from .rule import TOMLRule
 from .rule_formatter import toml_write
+from .rule_loader import RuleCollection
 from .schemas import CurrentSchema, available_versions
 from .utils import get_path, clear_caches, load_rule_contents
-
 
 RULES_DIR = get_path('rules')
 
@@ -59,15 +60,14 @@ def create_rule(path, config, required_only, rule_type):
 @click.pass_context
 def generate_rules_index(ctx: click.Context, query, overwrite, save_files=True):
     """Generate enriched indexes of rules, based on a KQL search, for indexing/importing into elasticsearch/kibana."""
-    from . import rule_loader
     from .packaging import load_current_package_version, Package
 
     if query:
         rule_paths = [r['file'] for r in ctx.invoke(search_rules, query=query, verbose=False)]
-        rules = rule_loader.load_rules(rule_loader.load_rule_files(paths=rule_paths, verbose=False), verbose=False)
-        rules = rules.values()
+        rules = RuleCollection()
+        rules.load_files(Path(p) for p in rule_paths)
     else:
-        rules = rule_loader.load_rules(verbose=False).values()
+        rules = RuleCollection.default()
 
     rule_count = len(rules)
     package = Package(rules, load_current_package_version(), verbose=False)
@@ -88,12 +88,12 @@ def generate_rules_index(ctx: click.Context, query, overwrite, save_files=True):
 
 
 @root.command('import-rules')
-@click.argument('infile', type=click.Path(dir_okay=False, exists=True), nargs=-1, required=False)
+@click.argument('input-file', type=click.Path(dir_okay=False, exists=True), nargs=-1, required=False)
 @click.option('--directory', '-d', type=click.Path(file_okay=False, exists=True), help='Load files from a directory')
-def import_rules(infile, directory):
+def import_rules(input_file, directory):
     """Import rules from json, toml, or Kibana exported rule file(s)."""
     rule_files = glob.glob(os.path.join(directory, '**', '*.*'), recursive=True) if directory else []
-    rule_files = sorted(set(rule_files + list(infile)))
+    rule_files = sorted(set(rule_files + list(input_file)))
 
     rule_contents = []
     for rule_file in rule_files:
@@ -117,22 +117,16 @@ def import_rules(infile, directory):
 def toml_lint(rule_file):
     """Cleanup files with some simple toml formatting."""
     if rule_file:
-        rules = list(rule_loader.load_rules(rule_loader.load_rule_files(paths=[rule_file])).values())
+        rules = RuleCollection()
+        rules.load_files(Path(p) for p in rule_file)
     else:
-        rules = list(rule_loader.load_rules().values())
-
-    # removed unneeded defaults
-    # TODO: we used to remove "unneeded" defaults, but this is a potentially tricky thing.
-    #       we need to figure out if a default is Kibana-imposed or detection-rules imposed.
-    #       ideally, we can explicitly mention default in TOML if desired and have a concept
-    #       of build-time defaults, so that defaults are filled in as late as possible
+        rules = RuleCollection.default()
 
     # re-save the rules to force TOML reformatting
     for rule in rules:
         rule.save_toml()
 
-    rule_loader.reset()
-    click.echo('Toml file linting complete')
+    click.echo('TOML file linting complete')
 
 
 @root.command('mass-update')
@@ -146,8 +140,10 @@ def toml_lint(rule_file):
 @click.pass_context
 def mass_update(ctx, query, metadata, language, field):
     """Update multiple rules based on eql results."""
+    rules = RuleCollection().default()
     results = ctx.invoke(search_rules, query=query, language=language, verbose=False)
-    rules = [rule_loader.get_rule(r['rule_id'], verbose=False) for r in results]
+    matching_ids = set(r["rule_id"] for r in results)
+    rules = rules.filter(lambda r: r.id in matching_ids)
 
     for rule in rules:
         for key, value in field:
@@ -161,41 +157,21 @@ def mass_update(ctx, query, metadata, language, field):
 
 
 @root.command('view-rule')
-@click.argument('rule-id', required=False)
-@click.option('--rule-file', '-f', type=click.Path(dir_okay=False), help='Optionally view a rule from a specified file')
+@click.argument('rule-file')
 @click.option('--api-format/--rule-format', default=True, help='Print the rule in final api or rule format')
 @click.pass_context
-def view_rule(ctx, rule_id, rule_file, api_format, verbose=True):
+def view_rule(ctx, rule_file, api_format):
     """View an internal rule or specified rule file."""
-    rule = None
+    rule = RuleCollection().load_file(rule_file)
 
-    if rule_id:
-        rule = rule_loader.get_rule(rule_id, verbose=False)
-    elif rule_file:
-        contents = {k: v for k, v in load_rule_contents(rule_file, single_only=True)[0].items() if v}
-
-        try:
-            rule = TOMLRule(rule_file, contents)
-        except jsonschema.ValidationError as e:
-            client_error(f'Rule: {rule_id or os.path.basename(rule_file)} failed validation', e, ctx=ctx)
+    if api_format:
+        click.echo(json.dumps(rule.contents.to_api_format(), indent=2, sort_keys=True))
     else:
-        client_error('Unknown rule!')
-
-    if not rule:
-        client_error('Unknown format!')
-
-    if verbose:
-        click.echo(toml_write(rule.rule_format()) if not api_format else
-                   json.dumps(rule.get_payload(), indent=2, sort_keys=True))
-
-    return rule
+        click.echo(toml_write(rule.contents.to_dict()))
 
 
 @root.command('export-rules')
-@click.argument('rule-id', nargs=-1, required=False)
-@click.option('--rule-file', '-f', multiple=True, type=click.Path(dir_okay=False), help='Export specified rule files')
-@click.option('--directory', '-d', multiple=True, type=click.Path(file_okay=False),
-              help='Recursively export rules from a directory')
+@multi_collection
 @click.option('--outfile', '-o', default=get_path('exports', f'{time.strftime("%Y%m%dT%H%M%SL")}.ndjson'),
               type=click.Path(dir_okay=False), help='Name of file for exported rules')
 @click.option('--replace-id', '-r', is_flag=True, help='Replace rule IDs with new IDs before export')
@@ -204,46 +180,22 @@ def view_rule(ctx, rule_id, rule_file, api_format, verbose=True):
 @click.option('--skip-unsupported', '-s', is_flag=True,
               help='If `--stack-version` is passed, skip rule types which are unsupported '
                    '(an error will be raised otherwise)')
-def export_rules(rule_id, rule_file, directory, outfile, replace_id, stack_version, skip_unsupported):
+def export_rules(rules, outfile, replace_id, stack_version, skip_unsupported):
     """Export rule(s) into an importable ndjson file."""
     from .packaging import Package
 
-    if not (rule_id or rule_file or directory):
-        client_error('Required: at least one of --rule-id, --rule-file, or --directory')
-
-    if rule_id:
-        all_rules = {r.id: r for r in rule_loader.load_rules(verbose=False).values()}
-        missing = [rid for rid in rule_id if rid not in all_rules]
-
-        if missing:
-            client_error(f'Unknown rules for rule IDs: {", ".join(missing)}')
-
-        rules = [r for r in all_rules.values() if r.id in rule_id]
-        rule_ids = [r.id for r in rules]
-    else:
-        rules = []
-        rule_ids = []
-
-    rule_files = list(rule_file)
-    for dirpath in directory:
-        rule_files.extend(list(Path(dirpath).rglob('*.toml')))
-
-    file_lookup = rule_loader.load_rule_files(verbose=False, paths=rule_files)
-    rules_from_files = rule_loader.load_rules(file_lookup=file_lookup).values() if file_lookup else []
-
-    # rule_loader.load_rules handles checks for duplicate rule IDs - this means rules loaded by ID are de-duped and
-    #   rules loaded from files and directories are de-duped from each other, so this check is to ensure that there is
-    #   no overlap between the two sets of rules
-    duplicates = [r.id for r in rules_from_files if r.id in rule_ids]
-    if duplicates:
-        client_error(f'Duplicate rules for rule IDs: {", ".join(duplicates)}')
-
-    rules.extend(rules_from_files)
+    assert len(rules) > 0, "No rules found"
 
     if replace_id:
-        from uuid import uuid4
-        for rule in rules:
-            rule.contents['rule_id'] = str(uuid4())
+        # if we need to replace the id, take each rule object and create a copy
+        # of it, with only the rule_id field changed
+        old_rules = rules
+        rules = RuleCollection()
+
+        for rule in old_rules:
+            new_data = dataclasses.replace(rule.contents.data, rule_id=str(uuid4()))
+            new_contents = dataclasses.replace(rule.contents, data=new_data)
+            rules.add_rule(TOMLRule(contents=new_contents))
 
     Path(outfile).parent.mkdir(exist_ok=True)
     package = Package(rules, '_', verbose=False)
@@ -252,29 +204,19 @@ def export_rules(rule_id, rule_file, directory, outfile, replace_id, stack_versi
 
 
 @root.command('validate-rule')
-@click.argument('rule-id', required=False)
-@click.option('--rule-name', '-n')
-@click.option('--path', '-p', type=click.Path(dir_okay=False))
+@click.argument('path')
 @click.pass_context
-def validate_rule(ctx, rule_id, rule_name, path):
+def validate_rule(ctx, path):
     """Check if a rule staged in rules dir validates against a schema."""
-    try:
-        rule = rule_loader.get_rule(rule_id, rule_name, path, verbose=False)
-        if not rule:
-            client_error('Rule not found!')
-
-        rule.validate(as_rule=True)
-        click.echo('Rule validation successful')
-        return rule
-    except jsonschema.ValidationError as e:
-        client_error(e.args[0], e, ctx=ctx)
+    rule = RuleCollection().load_file(Path(path))
+    click.echo('Rule validation successful')
+    return rule
 
 
 @root.command('validate-all')
-@click.option('--fail/--no-fail', default=True, help='Fail on first failure or process through all printing errors.')
 def validate_all(fail):
     """Check if all rules validates against a schema."""
-    rule_loader.load_rules(verbose=True, error=fail)
+    RuleCollection.default()
     click.echo('Rule validation successful')
 
 
@@ -292,7 +234,7 @@ def search_rules(query, columns, language, count, verbose=True, rules: Dict[str,
     from eql.pipes import CountPipe
 
     flattened_rules = []
-    rules = rules or rule_loader.load_rule_files(verbose=verbose)
+    rules = rules or {str(rule.path): rule for rule in RuleCollection.default()}
 
     for file_name, rule_doc in rules.items():
         flat = {"file": os.path.relpath(file_name)}

detection_rules/cli_utils.py
@@ -7,17 +7,93 @@ import copy
 import datetime
 import os
 from pathlib import Path
+from typing import List
 
 import click
 
 import kql
+import functools
 from . import ecs
 from .attack import matrix, tactics, build_threat_map_entry
 from .rule import TOMLRule, TOMLRuleContents
+from .rule_loader import RuleCollection, DEFAULT_RULES_DIR, dict_filter
 from .schemas import CurrentSchema
 from .utils import clear_caches, get_path
 
 RULES_DIR = get_path("rules")
+
+
+def single_collection(f):
+    """Add arguments to get a RuleCollection by file, directory or a list of IDs"""
+    from .misc import client_error
+
+    @click.option('--rule-file', '-f', multiple=False, required=False, type=click.Path(dir_okay=False))
+    @click.option('--rule-id', '-id', multiple=False, required=False)
+    @functools.wraps(f)
+    def get_collection(*args, **kwargs):
+        rule_name: List[str] = kwargs.pop("rule_name", [])
+        rule_id: List[str] = kwargs.pop("rule_id", [])
+        rule_files: List[str] = kwargs.pop("rule_file")
+        directories: List[str] = kwargs.pop("directory")
+
+        rules = RuleCollection()
+
+        if bool(rule_name) + bool(rule_id) + bool(rule_files) != 1:
+            client_error('Required: exactly one of --rule-id, --rule-file, or --directory')
+
+        rules.load_files(Path(p) for p in rule_files)
+        rules.load_directories(Path(d) for d in directories)
+
+        if rule_id:
+            rules.load_directory(DEFAULT_RULES_DIR, toml_filter=dict_filter(rule__rule_id=rule_id))
+
+            if len(rules) != 1:
+                client_error(f"Could not find rule with ID {rule_id}")
+
+        kwargs["rules"] = rules
+        return f(*args, **kwargs)
+
+    return get_collection
+
+
+def multi_collection(f):
+    """Add arguments to get a RuleCollection by file, directory or a list of IDs"""
+    from .misc import client_error
+
+    @click.option('--rule-file', '-f', multiple=True, type=click.Path(dir_okay=False), required=False)
+    @click.option('--directory', '-d', multiple=True, type=click.Path(file_okay=False), required=False,
+                  help='Recursively export rules from a directory')
+    @click.option('--rule-id', '-id', multiple=True, required=False)
+    @functools.wraps(f)
+    def get_collection(*args, **kwargs):
+        rule_name: List[str] = kwargs.pop("rule_name", [])
+        rule_id: List[str] = kwargs.pop("rule_id", [])
+        rule_files: List[str] = kwargs.pop("rule_file")
+        directories: List[str] = kwargs.pop("directory")
+
+        rules = RuleCollection()
+
+        if not rule_name or rule_id or rule_files:
+            client_error('Required: at least one of --rule-id, --rule-file, or --directory')
+
+        rules.load_files(Path(p) for p in rule_files)
+        rules.load_directories(Path(d) for d in directories)
+
+        if rule_id:
+            rules.load_directory(DEFAULT_RULES_DIR, toml_filter=dict_filter(rule__rule_id=rule_id))
+            found_ids = {rule.id for rule in rules}
+            missing = set(rule_id).difference(found_ids)
+
+            if missing:
+                client_error(f'Could not find rules with IDs: {", ".join(missing)}')
+
+        if len(rules) == 0:
+            client_error("No rules found")
+
+        kwargs["rules"] = rules
+        return f(*args, **kwargs)
+
+    return get_collection
 
 
 def rule_prompt(path=None, rule_type=None, required_only=True, save=True, verbose=False, **kwargs) -> TOMLRule:

detection_rules/devtools.py
@@ -17,15 +17,16 @@ from pathlib import Path
 import click
 from elasticsearch import Elasticsearch
 from eql import load_dump
-from kibana.connector import Kibana
 
+from kibana.connector import Kibana
 from . import rule_loader
+from .cli_utils import single_collection
 from .eswrap import CollectEvents, add_range_to_dsl
 from .main import root
 from .misc import PYTHON_LICENSE, add_client, GithubClient, Manifest, client_error, getdefault
 from .packaging import PACKAGE_FILE, Package, manage_versions, RELEASE_DIR
-from .rule import TOMLRule, TOMLRuleContents, BaseQueryRuleData
-from .rule_loader import get_rule
+from .rule import TOMLRule, BaseQueryRuleData
+from .rule_loader import production_filter, RuleCollection
 from .utils import get_path, dict_hash
 
 RULES_DIR = get_path('rules')
@@ -68,7 +69,7 @@ def update_lock_versions(rule_ids):
     if not click.confirm('Are you sure you want to update hashes without a version bump?'):
         return
 
-    rules = [r for r in rule_loader.load_rules(verbose=False).values() if r.id in rule_ids]
+    rules = RuleCollection.default().filter(lambda r: r.id in rule_ids)
     changed, new = manage_versions(rules, exclude_version_update=True, add_new=False, save_changes=True)
 
     if not changed:
@@ -86,10 +87,12 @@ def kibana_diff(rule_id, repo, branch, threads):
     """Diff rules against their version represented in kibana if exists."""
     from .misc import get_kibana_rules
 
+    rules = RuleCollection.default()
+
     if rule_id:
-        rules = {r.id: r for r in rule_loader.load_rules(verbose=False).values() if r.id in rule_id}
+        rules = rules.filter(lambda r: r.id in rule_id)
     else:
-        rules = {r.id: r for r in rule_loader.get_production_rules()}
+        rules = rules.filter(production_filter)
 
     # add versions to the rules
     manage_versions(list(rules.values()), verbose=False)
@@ -102,13 +105,13 @@ def kibana_diff(rule_id, repo, branch, threads):
     missing_from_kibana = list(set(repo_hashes).difference(set(kibana_hashes)))
 
     rule_diff = []
-    for rid, rhash in repo_hashes.items():
-        if rid in missing_from_kibana:
+    for rule_id, rule_hash in repo_hashes.items():
+        if rule_id in missing_from_kibana:
             continue
-        if rhash != kibana_hashes[rid]:
+        if rule_hash != kibana_hashes[rule_id]:
             rule_diff.append(
-                f'versions - repo: {rules[rid].contents["version"]}, kibana: {kibana_rules[rid]["version"]} -> '
-                f'{rid} - {rules[rid].name}'
+                f'versions - repo: {rules[rule_id].contents["version"]}, kibana: {kibana_rules[rule_id]["version"]} -> '
+                f'{rule_id} - {rules[rule_id].name}'
             )
 
     diff = {
@@ -373,8 +376,7 @@ def event_search(query, index, language, date_range, count, max_results, verbose
 
 
 @test_group.command('rule-event-search')
-@click.argument('rule-file', type=click.Path(dir_okay=False), required=False)
-@click.option('--rule-id', '-id')
+@single_collection
 @click.option('--date-range', '-d', type=(str, str), default=('now-7d', 'now'), help='Date range to scope search')
 @click.option('--count', '-c', is_flag=True, help='Return count of results only')
 @click.option('--max-results', '-m', type=click.IntRange(1, 1000), default=100,
@@ -382,17 +384,9 @@ def event_search(query, index, language, date_range, count, max_results, verbose
 @click.option('--verbose', '-v', is_flag=True)
 @click.pass_context
 @add_client('elasticsearch')
-def rule_event_search(ctx, rule_file, rule_id, date_range, count, max_results, verbose,
+def rule_event_search(ctx, rule, date_range, count, max_results, verbose,
                       elasticsearch_client: Elasticsearch = None):
     """Search using a rule file against an Elasticsearch instance."""
-    rule: TOMLRule
-
-    if rule_id:
-        rule = get_rule(rule_id, verbose=False)
-    elif rule_file:
-        rule = TOMLRule(path=rule_file, contents=TOMLRuleContents.from_dict(load_dump(rule_file)))
-    else:
-        client_error('Must specify a rule file or rule ID')
 
     if isinstance(rule.contents.data, BaseQueryRuleData):
         if verbose:
@@ -431,18 +425,17 @@ def rule_survey(ctx: click.Context, query, date_range, dump_file, hide_zero_coun
     """Survey rule counts."""
     from eql.table import Table
     from kibana.resources import Signal
-    from . import rule_loader
     from .main import search_rules
 
     survey_results = []
     start_time, end_time = date_range
 
     if query:
-        rule_paths = [r['file'] for r in ctx.invoke(search_rules, query=query, verbose=False)]
-        rules = rule_loader.load_rules(rule_loader.load_rule_files(paths=rule_paths, verbose=False), verbose=False)
-        rules = rules.values()
+        rules = RuleCollection()
+        paths = [Path(r['file']) for r in ctx.invoke(search_rules, query=query, verbose=False)]
+        rules.load_files(paths)
     else:
-        rules = rule_loader.load_rules(verbose=False).values()
+        rules = RuleCollection.default().filter(production_filter)
 
     click.echo(f'Running survey against {len(rules)} rules')
     click.echo(f'Saving detailed dump to: {dump_file}')

detection_rules/eswrap.py
@@ -7,8 +7,8 @@
 import json
 import os
 import time
-from contextlib import contextmanager
 from collections import defaultdict
+from contextlib import contextmanager
 from pathlib import Path
 from typing import Union
 
@@ -20,10 +20,9 @@ from elasticsearch.client import AsyncSearchClient, IngestClient, LicenseClient,
 import kql
 from .main import root
 from .misc import add_params, client_error, elasticsearch_options
-from .utils import format_command_options, normalize_timing_and_sort, unix_time_to_formatted, get_path
 from .rule import TOMLRule
-from .rule_loader import get_rule, rta_mappings
-
+from .rule_loader import rta_mappings, RuleCollection
+from .utils import format_command_options, normalize_timing_and_sort, unix_time_to_formatted, get_path
 
 COLLECTION_DIR = get_path('collections')
 MATCH_ALL = {'bool': {'filter': [{'match_all': {}}]}}
@@ -88,7 +87,8 @@ class RtaEvents(object):
         """Evaluate a rule against collected events and update mapping."""
         from .utils import combine_sources, evaluate
 
-        rule = get_rule(rule_id, verbose=False)
+        rule = next((rule for rule in RuleCollection.default() if rule.id == rule_id), None)
+        assert rule is not None, f"Unable to find rule with ID {rule_id}"
         merged_events = combine_sources(*self.events.values())
         filtered = evaluate(rule, merged_events)
 

detection_rules/kbwrap.py
@@ -4,13 +4,16 @@
 # 2.0.
 
 """Kibana cli commands."""
+import uuid
+
 import click
+
 import kql
 from kibana import Kibana, Signal, RuleResource
-
+from .cli_utils import multi_collection
 from .main import root
 from .misc import add_params, client_error, kibana_options
-from .rule_loader import load_rule_files, load_rules
+from .schemas import downgrade
 from .utils import format_command_options
 
 
@@ -49,30 +52,28 @@ def kibana_group(ctx: click.Context, **kibana_kwargs):
 
 
 @kibana_group.command("upload-rule")
-@click.argument("toml-files", nargs=-1, required=True)
+@multi_collection
 @click.option('--replace-id', '-r', is_flag=True, help='Replace rule IDs with new IDs before export')
 @click.pass_context
-def upload_rule(ctx, toml_files, replace_id):
+def upload_rule(ctx, rules, replace_id):
     """Upload a list of rule .toml files to Kibana."""
-    from .packaging import manage_versions
 
     kibana = ctx.obj['kibana']
-    file_lookup = load_rule_files(paths=toml_files)
-    rules = list(load_rules(file_lookup=file_lookup).values())
-
-    # assign the versions from etc/versions.lock.json
-    # rules that have changed in hash get incremented, others stay as-is.
-    # rules that aren't in the lookup default to version 1
-    manage_versions(rules, verbose=False)
-
     api_payloads = []
 
     for rule in rules:
         try:
-            payload = rule.get_payload(include_version=True, replace_id=replace_id, embed_metadata=True,
-                                       target_version=kibana.version)
+            payload = rule.contents.to_api_format()
+            payload.setdefault("meta", {}).update(rule.contents.metadata.to_dict())
+
+            if replace_id:
+                payload["rule_id"] = str(uuid.uuid4())
+
+            payload = downgrade(payload, target_version=kibana.version)
+
         except ValueError as e:
             client_error(f'{e} in version:{kibana.version}, for rule: {rule.name}', e, ctx=ctx)
+
         rule = RuleResource(payload)
         api_payloads.append(rule)
 

detection_rules/mappings.py
@@ -10,7 +10,6 @@ from collections import defaultdict
 from .schemas import validate_rta_mapping
 from .utils import load_etc_dump, save_etc_dump, get_path
 
-
 RTA_DIR = get_path("rta")
 
 

detection_rules/mixins.py
@@ -35,6 +35,10 @@ class MarshmallowDataclassMixin:
         """Get the marshmallow schema for the data class"""
         return marshmallow_dataclass.class_schema(cls)()
 
+    def get(self, key: str):
+        """Get a key from the query data without raising attribute errors."""
+        return getattr(self, key, None)
+
     @classmethod
     def from_dict(cls: Type[ClassT], obj: dict) -> ClassT:
         """Deserialize and validate a dataclass from a dict using marshmallow."""

detection_rules/packaging.py
@@ -17,10 +17,10 @@ from typing import List, Optional, Tuple
 import click
 import yaml
 
-from . import rule_loader
 from .misc import JS_LICENSE, cached
-from .rule import TOMLRule, BaseQueryRuleData, RULES_DIR, ThreatMapping
+from .rule import TOMLRule, BaseQueryRuleData, ThreatMapping
 from .rule import downgrade_contents_from_rule
+from .rule_loader import RuleCollection, DEFAULT_RULES_DIR
 from .schemas import CurrentSchema, definitions
 from .utils import Ndjson, get_path, get_etc_path, load_etc_dump, save_etc_dump
 
@@ -325,7 +325,7 @@ class Package(object):
     @classmethod
     def from_config(cls, config: dict = None, update_version_lock: bool = False, verbose: bool = False) -> 'Package':
         """Load a rules package given a config."""
-        all_rules = rule_loader.load_rules(verbose=False).values()
+        all_rules = RuleCollection.default()
         config = config or {}
         exclude_fields = config.pop('exclude_fields', {})
         log_deprecated = config.pop('log_deprecated', False)
@@ -335,20 +335,13 @@ class Package(object):
         if log_deprecated:
             deprecated_rules = [r for r in all_rules if r.contents.metadata.maturity == 'deprecated']
 
-        rules = list(filter(lambda rule: filter_rule(rule, rule_filter, exclude_fields), all_rules))
+        rules = all_rules.filter(lambda r: filter_rule(r, rule_filter, exclude_fields))
 
         if verbose:
             click.echo(f' - {len(all_rules) - len(rules)} rules excluded from package')
 
-        update = config.pop('update', {})
         package = cls(rules, deprecated_rules=deprecated_rules, update_version_lock=update_version_lock,
                       verbose=verbose, **config)
-
-        # Allow for some fields to be overwritten
-        if update.get('data', {}):
-            for rule in package.rules:
-                for sub_dict, values in update.items():
-                    rule.contents[sub_dict].update(values)
 
         return package
 
@@ -551,7 +544,7 @@ class Package(object):
                             status=status,
                             package_version=self.name,
                             flat_mitre=ThreatMapping.flatten(rule.contents.data.threat).to_dict(),
-                            relative_path=str(rule.path.resolve().relative_to(RULES_DIR)))
+                            relative_path=str(rule.path.resolve().relative_to(DEFAULT_RULES_DIR)))
             bulk_upload_docs.append(rule_doc)
             importable_rules_docs.append(rule_doc)
 

commit e0d847359b7e9a61d28da768e1e89aa94d52e9d6

bug fix: remove unused param caused by commit 6ed1a39efeb444222694b0a8e92d72f8d77bd6b0

detection_rules/main.py
@@ -250,7 +250,7 @@ def validate_rule(ctx, path):
 
 
 @root.command('validate-all')
-def validate_all(fail):
+def validate_all():
     """Check if all rules validates against a schema."""
     RuleCollection.default()
     click.echo('Rule validation successful')