c072695f3c4fc0c092b35d432df703f1f2af62b9
Terrance DeJesus <terrance.dejesus@elastic.co>
2022-05-26 12:55:13 -0400

    linting issue addressed in schemas _init__

--- detection_rules/schemas/__init__.py
+++ detection_rules/schemas/__init__.py
@@ -196,6 +196,7 @@ def migrate_to_8_2(version: Version, api_contents: dict) -> dict:
     """Default migration for 8.2."""
     return strip_additional_properties(version, api_contents)
 
+
 @migrate("8.3")
 def migrate_to_8_3(version: Version, api_contents: dict) -> dict:
     """Default migration for 8.3."""
