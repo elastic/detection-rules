a65ec294f0449e7363a5989e53f4dc70ffe54f7f
Justin Ibarra <brokensound77@users.noreply.github.com>
2022-09-01 09:16:39 -0600

    Update RTA common.py for py3 (#2287)
    * add run-all argument and initial p2 conversion
    
    * remove unicode
    
    * format with black
    
    (cherry picked from commit 0fc8006e7a02e0f6302480132c0d77bd34adfd8b)

--- rta/__main__.py
+++ rta/__main__.py
@@ -5,18 +5,57 @@
 
 import argparse
 import importlib
-import os
+import subprocess
+import sys
+import time
+from pathlib import Path
 
-from . import get_ttp_names
+from . import get_ttp_list, get_ttp_names
+from .common import CURRENT_OS
 
-parser = argparse.ArgumentParser("rta")
-parser.add_argument("ttp_name")
 
-parsed_args, remaining = parser.parse_known_args()
-ttp_name, _ = os.path.splitext(os.path.basename(parsed_args.ttp_name))
+DELAY = 1
 
-if ttp_name not in get_ttp_names():
-    raise ValueError("Unknown RTA {}".format(ttp_name))
 
-module = importlib.import_module("rta." + ttp_name)
-exit(module.main(*remaining))
+def run_all():
+    """Run a single RTA."""
+    errors = []
+    for ttp_file in get_ttp_list(CURRENT_OS):
+        print(f"---- {Path(ttp_file).name} ----")
+        p = subprocess.Popen([sys.executable, ttp_file])
+        p.wait()
+        code = p.returncode
+
+        if p.returncode:
+            errors.append((ttp_file, code))
+
+        time.sleep(DELAY)
+        print("")
+
+    return len(errors)
+
+
+def run(ttp_name: str, *args):
+    """Run all RTAs compatible with OS."""
+    if ttp_name not in get_ttp_names():
+        raise ValueError(f"Unknown RTA {ttp_name}")
+
+    module = importlib.import_module("rta." + ttp_name)
+    return module.main(*args)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("rta")
+    parser.add_argument("--ttp-name")
+    parser.add_argument("--run-all", action="store_true")
+    parser.add_argument("--delay", type=int, help="For run-all, the delay between executions")
+    parsed_args, remaining = parser.parse_known_args()
+
+    if parsed_args.ttp_name and parsed_args.run_all:
+        raise ValueError(f"Pass --ttp-name or --run-all, not both")
+
+    if parsed_args.run_all:
+        exit(run_all())
+    else:
+        rta_name = Path(parsed_args.run).stem
+        exit(run(rta_name, *remaining))
