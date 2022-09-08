6299b5c60e6d19269155afccac66638a146723c5
Mika Ayenson <Mikaayenson@users.noreply.github.com>
2022-09-08 12:50:39 -0400

    Release ER Production RTAs to DR (#2270)
    (cherry picked from commit 0358ec9d9a5e05c538cd4961f210bf544bddc53f)

--- detection_rules/main.py
+++ detection_rules/main.py
@@ -17,7 +17,9 @@ from uuid import uuid4
 
 import click
 
+
 from .cli_utils import rule_prompt, multi_collection
+from .mappings import build_coverage_map, get_triggered_rules, print_converage_summary
 from .misc import add_client, client_error, nested_set, parse_config, load_current_package_version
 from .rule import TOMLRule, TOMLRuleContents
 from .rule_formatter import toml_write
@@ -425,3 +427,29 @@ def prep_rule(author: str):
     updated_rule.write_text(json.dumps(template_rule, sort_keys=True))
     click.echo(f'Rule saved to: {updated_rule}. Import this to Kibana to create alerts on all dnstwist-* indexes')
     click.echo('Note: you only need to import and enable this rule one time for all dnstwist-* indexes')
+
+
+@root.group('rta')
+def rta_group():
+    """Commands related to Red Team Automation (RTA) scripts."""
+
+
+# create command to show rule-rta coverage
+@rta_group.command('coverage')
+@click.option("-o", "--os-filter", default="all",
+              help="Filter rule coverage summary by OS. (E.g. windows) Default: all")
+def rta_coverage(os_filter: str):
+    """Show coverage of RTA / rules by os type."""
+
+    # get all rules
+    all_rules = RuleCollection.default()
+
+    # get rules triggered by RTA
+    triggered_rules = get_triggered_rules()
+
+    # build coverage map
+    coverage_map = build_coverage_map(triggered_rules, all_rules)
+
+    # # print summary
+    all_rule_count = len(all_rules.rules)
+    print_converage_summary(coverage_map, all_rule_count, os_filter)
--- rta/scrobj_com_hijack.py
+++ rta/scrobj_com_hijack.py
@@ -9,9 +9,19 @@
 # Description: Modifies the Registry to create a new user-defined COM broker, "scrobj.dll".
 
 from . import common
+from . import RtaMetadata
 
 
-@common.requires_os(common.WINDOWS)
+metadata = RtaMetadata(
+    uuid="ac739578-c978-429f-9454-0bbe82f993f4",
+    platforms=["windows"],
+    endpoint=[],
+    siem=[{"rule_id": "16a52c14-7883-47af-8745-9357803f0d4c", "rule_name": "Component Object Model Hijacking"}],
+    techniques=["T1546"],
+)
+
+
+@common.requires_os(metadata.platforms)
 def main():
     key = "SOFTWARE\\Classes\\CLSID\\{00000000-0000-0000-0000-0000DEADBEEF}"
     subkey = "InprocServer32"
