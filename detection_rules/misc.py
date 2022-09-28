ca9895a743eb40a1623c458a455f21aca7dfe57e
Terrance DeJesus <99630311+terrancedejesus@users.noreply.github.com>
2022-09-28 11:43:13 -0400

    Update detection_rules/misc.py
    Co-authored-by: Mika Ayenson <Mikaayenson@users.noreply.github.com>

--- detection_rules/misc.py
+++ detection_rules/misc.py
@@ -265,7 +265,7 @@ def get_kibana_rules(*rule_paths, repo='elastic/kibana', branch='master', verbos
 
 
 @cached
-def load_current_package_version(patch=False) -> str:
+def load_current_package_version(patch: bool = False) -> str:
     """Load the current package version from config file."""
     current_package_version = load_etc_dump('packages.yml')['package']['name']
     current_package_version += ".0" if patch else current_package_version
