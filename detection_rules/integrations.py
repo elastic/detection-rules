ff7ec1d8d914d1220fb6dee9be3a2d99fc64f45a
Terrance DeJesus <99630311+terrancedejesus@users.noreply.github.com>
2022-09-28 09:33:49 -0400

    [FR] Re-factor Build Integrations Manifest (#2274)
    * adjusted how integrations list is created
    
    * removed unused import and addressed linting errors
    
    * adjusted integration_manifest dictionary to only load latest major
    
    * adjusted manifests sourcing from GH to EPR CDN
    
    * addressed flake errors
    
    * added some additional comments and formatting
    
    * updaing integration-manifests file
    
    * adjusted test_integration testing
    
    * addressed flake errors
    
    * Update detection_rules/integrations.py
    
    Co-authored-by: Justin Ibarra <brokensound77@users.noreply.github.com>
    
    * Update detection_rules/integrations.py
    
    Co-authored-by: Justin Ibarra <brokensound77@users.noreply.github.com>
    
    * added folder unit tests
    
    * updated unit test to remove network calls
    
    * Update tests/test_all_rules.py
    
    Co-authored-by: Mika Ayenson <Mikaayenson@users.noreply.github.com>
    
    Co-authored-by: Justin Ibarra <brokensound77@users.noreply.github.com>
    Co-authored-by: Mika Ayenson <Mikaayenson@users.noreply.github.com>
    
    (cherry picked from commit b31a1b761c86ec3f4a98564b87a10743b43cd33f)

--- detection_rules/integrations.py
+++ detection_rules/integrations.py
@@ -11,12 +11,11 @@ import re
 from pathlib import Path
 from typing import Union
 
-import yaml
+import requests
 from marshmallow import EXCLUDE, Schema, fields, post_load
 
-from .ghwrap import GithubClient
 from .semver import Version
-from .utils import INTEGRATION_RULE_DIR, cached, get_etc_path, read_gzip
+from .utils import cached, get_etc_path, read_gzip
 
 MANIFEST_FILE_PATH = Path(get_etc_path('integration-manifests.json.gz'))
 
@@ -34,7 +33,7 @@ class IntegrationManifestSchema(Schema):
     description = fields.Str(required=True)
     conditions = fields.Dict(required=True)
     policy_templates = fields.List(fields.Dict, required=True)
-    owner = fields.Dict(required=True)
+    owner = fields.Dict(required=False)
 
     @post_load
     def transform_policy_template(self, data, **kwargs):
@@ -42,28 +41,16 @@ class IntegrationManifestSchema(Schema):
         return data
 
 
-def build_integrations_manifest(token: str, overwrite: bool) -> None:
+def build_integrations_manifest(overwrite: bool, rule_integrations: list) -> None:
     """Builds a new local copy of manifest.yaml from integrations Github."""
     if overwrite:
         if os.path.exists(MANIFEST_FILE_PATH):
             os.remove(MANIFEST_FILE_PATH)
-    rule_integrations = [d.name for d in Path(INTEGRATION_RULE_DIR).glob('*') if d.is_dir()]
-    if "endpoint" in rule_integrations:
-        rule_integrations.remove("endpoint")
 
     final_integration_manifests = {integration: {} for integration in rule_integrations}
 
-    # initialize github client and point to package-storage prod
-    github = GithubClient(token)
-    client = github.authenticated_client
-    organization = client.get_organization("elastic")
-    repository = organization.get_repo("package-storage")
-    pkg_storage_prod_branch = repository.get_branch("production")
-    pkg_storage_branch_sha = pkg_storage_prod_branch.commit.sha
-
     for integration in rule_integrations:
-        integration_manifests = get_integration_manifests(repository, pkg_storage_branch_sha,
-                                                          pkg_path=f"packages/{integration}")
+        integration_manifests = get_integration_manifests(integration)
         for manifest in integration_manifests:
             validated_manifest = IntegrationManifestSchema(unknown=EXCLUDE).load(manifest)
             package_version = validated_manifest.pop("version")
@@ -72,12 +59,18 @@ def build_integrations_manifest(token: str, overwrite: bool) -> None:
     manifest_file = gzip.open(MANIFEST_FILE_PATH, "w+")
     manifest_file_bytes = json.dumps(final_integration_manifests).encode("utf-8")
     manifest_file.write(manifest_file_bytes)
+    print(f"final integrations manifests dumped: {MANIFEST_FILE_PATH}")
 
 
 def find_least_compatible_version(package: str, integration: str,
                                   current_stack_version: str, packages_manifest: dict) -> Union[str, None]:
     """Finds least compatible version for specified integration based on stack version supplied."""
     integration_manifests = {k: v for k, v in sorted(packages_manifest[package].items(), key=Version)}
+
+    # trim integration_manifests to only the latest major entries
+    max_major, *_ = max([Version(manifest_version) for manifest_version in integration_manifests])
+    latest_major_integration_manifests = \
+        {k: v for k, v in integration_manifests.items() if Version(k)[0] == max_major}
 
     def compare_versions(int_ver: str, pkg_ver: str) -> bool:
         """Compares integration and package version"""
@@ -90,33 +83,28 @@ def find_least_compatible_version(package: str, integration: str,
         compatible = Version(int_ver) <= Version(pkg_ver)
         return compatible
 
-    for version, manifest in integration_manifests.items():
-        for kibana_compat_vers in re.sub(r"\>|\<|\=|\^", "", manifest["conditions"]["kibana.version"]).split(" || "):
+    for version, manifest in latest_major_integration_manifests.items():
+        for kibana_compat_vers in re.sub(r"\>|\<|\=|\^", "", manifest["conditions"]["kibana"]["version"]).split(" || "):
             if compare_versions(kibana_compat_vers, current_stack_version):
-                return version
+                return f"^{version}"
     print(f"no compatible version for integration {package}:{integration}")
     return None
 
 
-def get_integration_manifests(repository, sha: str, pkg_path: str) -> list:
+def get_integration_manifests(integration: str) -> list:
     """Iterates over specified integrations from package-storage and combines manifests per version."""
-    integration = pkg_path.split("/")[-1]
-    versioned_packages = repository.get_dir_contents(pkg_path, ref=sha)
-    versions = [p.path.split("/")[-1] for p in versioned_packages]
+    epr_search_url = "https://epr.elastic.co/search"
 
-    manifests = []
-    for version in versions:
-        contents = repository.get_dir_contents(f"{pkg_path}/{version}", ref=sha)
-        print(f"Processing {integration} - Version: {version}")
+    # link for search parameters - https://github.com/elastic/package-registry
+    epr_search_parameters = {"package": f"{integration}", "prerelease": "true",
+                             "all": "true", "include_policy_templates": "true"}
+    epr_search_response = requests.get(epr_search_url, params=epr_search_parameters)
+    epr_search_response.raise_for_status()
+    manifests = epr_search_response.json()
 
-        processing_version = contents[0].path.split("/")[2]
-        manifest_content = [c for c in contents if "manifest" in c.path]
+    if not manifests:
+        raise ValueError(f"EPR search for {integration} integration package returned empty list")
 
-        if len(manifest_content) < 1:
-            raise Exception(f"manifest file does not exist for {integration}:{processing_version}")
-
-        path = manifest_content[0].path
-        manifest_content = yaml.safe_load(repository.get_contents(path, ref=sha).decoded_content.decode())
-        manifests.append(manifest_content)
-
+    print(f"loaded {integration} manifests from the following package versions: "
+          f"{[manifest['version'] for manifest in manifests]}")
     return manifests
