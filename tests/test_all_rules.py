6055d0db608542e3267b5f78db2d8d8791fb8176
Jonhnathan <26856693+w0rk3r@users.noreply.github.com>
2022-11-17 13:38:34 -0800

    [Security Content] Introduce Osquery Markdown Plugin Queries in Investigation Guides (#2387)
    * [Security Content] Introduce Osquery Markdown Plugin Queries in Investigation Guides
    
    * Remove min_stack and add Note
    
    * Fix Typo and preffix
    
    * Update command_and_control_certutil_network_connection.toml
    
    * Add unit test to check Note about Osquery Markdown plugin and Version limitations
    
    * Update test_all_rules.py
    
    * Update test_all_rules.py
    
    * Change Note Verbiage

--- tests/test_all_rules.py
+++ tests/test_all_rules.py
@@ -765,3 +765,19 @@ class TestRiskScoreMismatch(BaseRuleTest):
             err_msg = 'The following rules have mismatches between Severity and Risk Score field values:\n'
             err_msg += invalid_str
             self.fail(err_msg)
+
+
+class TestOsqueryPluginNote(BaseRuleTest):
+    """Test if a guide containing Osquery Plugin syntax contains the version note."""
+
+    def test_note_guide(self):
+        osquery_note = '> **Note**:\n'
+        osquery_note_pattern = osquery_note + '> This investigation guide uses the [Osquery Markdown Plugin]' \
+            '(https://www.elastic.co/guide/en/security/master/invest-guide-run-osquery.html) introduced in Elastic ' \
+            'stack version 8.5.0. Older Elastic stacks versions will see unrendered markdown in this guide.'
+
+        for rule in self.all_rules:
+            if rule.contents.data.note and "!{osquery" in rule.contents.data.note:
+                if osquery_note_pattern not in rule.contents.data.note:
+                    self.fail(f'{self.rule_str(rule)} Investigation guides using the Osquery Markdown must contain '
+                              f'the following note:\n{osquery_note_pattern}')
