a02edf268ef3c185cb2068802fe61cfaeaf9dbe4
Mika Ayenson <mika.ayenson@elastic.co>
2023-01-27 16:58:56 -0500

    validate rules with endgame index and add non-ecs-mapping to schema

--- detection_rules/rule_validators.py
+++ detection_rules/rule_validators.py
@@ -17,7 +17,7 @@ from .rule import QueryRuleData, QueryValidator, RuleMeta, TOMLRuleContents
 
 
 class KQLValidator(QueryValidator):
-    """Specific fields for query event types."""
+    """Specific fields for KQL query event types."""
 
     @cached_property
     def ast(self) -> kql.ast.Expression:
@@ -81,32 +81,27 @@ class KQLValidator(QueryValidator):
             integration_schema = integration_schema_data['schema']
             stack_version = integration_schema_data['stack_version']
 
+            # add non-ecs-schema fields for edge cases not added to the integration
+            for index_name in data.index:
+                integration_schema.update(**ecs.flatten(ecs.get_index_schema(index_name)))
+
             try:
                 # validate the query against the integration fields with the package version
                 kql.parse(self.query, schema=integration_schema)
             except kql.KqlParseError as exc:
-                # TODO: Uncomment this when integration schema validation is complete
-                # trailer = (f"\nTry adding event.module or event.dataset to specify integration module\n\n"
-                #            f"{package=}, {integration=}, {package_version=}, "
-                #            f"{stack_version=}, {ecs_version=}"
-                #            )
+                trailer = (f"\nTry adding event.module or event.dataset to specify integration module\n\n"
+                           f"{package=}, {integration=}, {package_version=}, "
+                           f"{stack_version=}, {ecs_version=}"
+                           )
 
+                # TODO: Remove print
+                print(f"\n\nError on {data.name}:\n\t{str(exc)}")
                 # raise kql.KqlParseError(exc.error_msg, exc.line, exc.column, exc.source,
-                #                         len(exc.caret.lstrip()), trailer=trailer) from Non
-
-                # TODO: Remove all below when integration schema validation is complete
-                errordata = {"rule_name": data.name, "rule_id": data.rule_id,
-                             "minstack_rule_version": meta.min_stack_version,
-                             "package": package, "integration": integration, "integration_version": package_version,
-                             "ecs_version": ecs_version, "stack_version": stack_version, "error": str(exc)}
-
-                print(errordata)
-                # with open("integration_schema_validation_errors.ndson", "a") as f:
-                #     json.dump(errordata, f)
-                #     f.write("\n")
+                #                         len(exc.caret.lstrip()), trailer=trailer) from None
 
 
 class EQLValidator(QueryValidator):
+    """Specific fields for EQL query event types."""
 
     @cached_property
     def ast(self) -> eql.ast.Expression:
@@ -124,7 +119,7 @@ class EQLValidator(QueryValidator):
     def unique_fields(self) -> List[str]:
         return list(set(str(f) for f in self.ast if isinstance(f, eql.ast.Field)))
 
-    def validate_query_with_schema(self, schema: Union[ecs.KqlSchema2Eql, endgame.EndgameSchema],
+    def validate_query_with_schema(self, data: 'QueryRuleData', schema: Union[ecs.KqlSchema2Eql, endgame.EndgameSchema],
                                    err_trailer: str, beat_types: list = None) -> None:
         """Validate the query against the schema."""
         try:
@@ -141,8 +136,11 @@ class EQLValidator(QueryValidator):
                     fields_str = ', '.join(text_fields)
                     trailer = f"\neql does not support text fields: {fields_str}\n\n{trailer}"
 
-            raise exc.__class__(exc.error_msg, exc.line, exc.column, exc.source,
-                                len(exc.caret.lstrip()), trailer=trailer) from None
+            # TODO: Remove print
+            print(f"\n\nError on {data.name}:\n\t{str(exc)}")
+            # raise exc.__class__(exc.error_msg, exc.line, exc.column, exc.source,
+            #                     len(exc.caret.lstrip()), trailer=trailer) from None
+
         except Exception:
             print(err_trailer)
             raise
@@ -161,6 +159,16 @@ class EQLValidator(QueryValidator):
                 # validate the query against related integration fields
                 print(f"Validating {data.name} against {len(package_integrations)} integration(s)")
                 self.validate_integration(data, meta, package_integrations)
+
+                # Still need to check endgame if it's in the index
+                for stack_version, mapping in meta.get_validation_stack_versions().items():
+                    endgame_version = mapping['endgame']
+                    endgame_schema = self.get_endgame_schema(data.index, endgame_version)
+                    if endgame_schema:
+                        # validate query against the endgame schema
+                        err_trailer = f'stack: {stack_version}, endgame: {endgame_version}'
+                        self.validate_query_with_schema(data=data, schema=endgame_schema, err_trailer=err_trailer)
+
             else:
                 for stack_version, mapping in meta.get_validation_stack_versions().items():
                     beats_version = mapping['beats']
@@ -175,11 +183,12 @@ class EQLValidator(QueryValidator):
                     eql_schema = ecs.KqlSchema2Eql(schema)
 
                     # validate query against the beats and eql schema
-                    self.validate_query_with_schema(schema=eql_schema, err_trailer=err_trailer, beat_types=beat_types)
+                    self.validate_query_with_schema(data=data, schema=eql_schema, err_trailer=err_trailer,
+                                                    beat_types=beat_types)
 
                     if endgame_schema:
                         # validate query against the endgame schema
-                        self.validate_query_with_schema(schema=endgame_schema, err_trailer=err_trailer)
+                        self.validate_query_with_schema(data=data, schema=endgame_schema, err_trailer=err_trailer)
 
     def validate_integration(self, data: QueryRuleData, meta: RuleMeta, package_integrations: List[dict]) -> None:
         """Validate an EQL query while checking TOMLRule against integration schemas."""
@@ -192,31 +201,22 @@ class EQLValidator(QueryValidator):
             integration = integration_schema_data['integration']
             package = integration_schema_data['package']
             package_version = integration_schema_data['package_version']
-            schema = integration_schema_data['schema']
+            integration_schema = integration_schema_data['schema']
             stack_version = integration_schema_data['stack_version']
 
-            eql_schema = ecs.KqlSchema2Eql(schema)
+            # add non-ecs-schema fields for edge cases not added to the integration
+            for index_name in data.index:
+                integration_schema.update(**ecs.flatten(ecs.get_index_schema(index_name)))
+
+            eql_schema = ecs.KqlSchema2Eql(integration_schema)
             err_trailer = f'stack: {stack_version}, integration: {integration},' \
                           f'ecs: {ecs_version}, package: {package}, package_version: {package_version}'
 
-            # validate query against the endgame schema
             try:
-                self.validate_query_with_schema(schema=eql_schema, err_trailer=err_trailer)
-            except Exception as exc:
-
-                # TODO: Uncomment this when integration schema validation is complete
+                self.validate_query_with_schema(data=data, schema=eql_schema, err_trailer=err_trailer)
+            except eql.EqlParseError as exc:
                 # raise exc
-
-                # TODO: Remove all below when integration schema validation is complete
-                errordata = {"rule_name": data.name, "rule_id": data.rule_id,
-                             "minstack_rule_version": meta.min_stack_version,
-                             "package": package, "integration": integration, "integration_version": package_version,
-                             "ecs_version": ecs_version, "stack_version": stack_version, "error": str(exc)}
-
-                print(errordata)
-                # with open("integration_schema_validation_errors.ndson", "a") as f:
-                #     json.dump(errordata, f)
-                #     f.write("\n")
+                pass
 
 
 def extract_error_field(exc: Union[eql.EqlParseError, kql.KqlParseError]) -> Optional[str]:
