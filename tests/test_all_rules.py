5239ff7384438fd7f3392b5f8878206f7145fa40
terrancedejesus <terrance.dejesus@elastic.co>
2023-01-29 08:30:24 -0500

    updated test_all_rules unit testing Version() references

--- tests/test_all_rules.py
+++ tests/test_all_rules.py
@@ -26,7 +26,7 @@ from rta import get_available_tests
 
 from .base import BaseRuleTest
 
-PACKAGE_STACK_VERSION = Version(current_stack_version()) + (0,)
+PACKAGE_STACK_VERSION = semver.VersionInfo(*current_stack_version().split("."))
 
 
 class TestValidRules(BaseRuleTest):
@@ -426,19 +426,18 @@ class TestRuleMetadata(BaseRuleTest):
         #           f'Re-add to the deprecated folder and update maturity to "deprecated": \n {missing_rule_strings}'
         # self.assertEqual([], missing_rules, err_msg)
 
-        stack_version = Version(current_stack_version())
         for rule_id, entry in deprecations.items():
             # if a rule is deprecated and not backported in order to keep the rule active in older branches, then it
             # will exist in the deprecated_rules.json file and not be in the _deprecated folder - this is expected.
             # However, that should not occur except by exception - the proper way to handle this situation is to
             # "fork" the existing rule by adding a new min_stack_version.
-            if stack_version < Version(entry['stack_version']):
+            if PACKAGE_STACK_VERSION < semver.VersionInfo(*entry['stack_version'].split(".")):
                 continue
 
             rule_str = f'{rule_id} - {entry["rule_name"]} ->'
             self.assertIn(rule_id, deprecated_rules, f'{rule_str} is logged in "deprecated_rules.json" but is missing')
 
-    @unittest.skipIf(PACKAGE_STACK_VERSION < Version("8.3.0"),
+    @unittest.skipIf(PACKAGE_STACK_VERSION < semver.VersionInfo.parse("8.3.0"),
                      "Test only applicable to 8.3+ stacks regarding related integrations build time field.")
     def test_integration_tag(self):
         """Test integration rules defined by metadata tag."""
@@ -610,7 +609,8 @@ class TestRuleTiming(BaseRuleTest):
             has_event_ingested = rule.contents.data.timestamp_override == 'event.ingested'
             indexes = rule.contents.data.get('index', [])
             beats_indexes = parse_beats_from_index(indexes)
-            min_stack_is_less_than_82 = Version(rule.contents.metadata.min_stack_version or '7.13') < (8, 2)
+            min_stack_is_less_than_82 = semver.VersionInfo.parse(rule.contents.metadata.min_stack_version or '7.13.0') \
+                                        < semver.VersionInfo.parse("8.2.0")
             config = rule.contents.data.get('note') or ''
             rule_str = self.rule_str(rule, trailer=None)
 
@@ -745,7 +745,7 @@ class TestBuildTimeFields(BaseRuleTest):
 
     def test_build_fields_min_stack(self):
         """Test that newly introduced build-time fields for a min_stack for applicable rules."""
-        current_stack_ver = Version(current_stack_version())
+        current_stack_ver = PACKAGE_STACK_VERSION
         invalids = []
 
         for rule in self.production_rules:
@@ -756,7 +756,7 @@ class TestBuildTimeFields(BaseRuleTest):
             for build_field, field_versions in build_fields.items():
                 start_ver, end_ver = field_versions
                 if start_ver is not None and current_stack_ver >= start_ver:
-                    if min_stack is None or not Version(min_stack) >= start_ver:
+                    if min_stack is None or not semver.VersionInfo.parse(min_stack) >= start_ver:
                         errors.append(f'{build_field} >= {start_ver}')
 
             if errors:
