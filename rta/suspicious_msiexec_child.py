8fb961241201dbacbda396c01a6c64c5e964732b
Jonhnathan <26856693+w0rk3r@users.noreply.github.com>
2023-03-14 12:46:39 -0300

    Update suspicious_msiexec_child.py

--- rta/suspicious_msiexec_child.py
+++ rta/suspicious_msiexec_child.py
@@ -10,10 +10,15 @@ from . import RtaMetadata
 metadata = RtaMetadata(
     uuid="2cefb7c2-5ffc-4410-a63c-bded93b258c3",
     platforms=["windows"],
-    endpoint=[{
-        'rule_id': '877c6bd9-8df1-4a15-aa97-2a091731b15d',
-        'rule_name': 'Suspicious MsiExec Child Process'
-    }],
+    endpoint=[
+        {
+            'rule_id': '877c6bd9-8df1-4a15-aa97-2a091731b15d',
+            'rule_name': 'Suspicious MsiExec Child Process'
+        },
+        {'rule_id': '16c84e67-e5e7-44ff-aefa-4d771bcafc0c', 'rule_name': 'Execution from Unusual Directory'},
+        {'rule_id': '35dedf0c-8db6-4d70-b2dc-a133b808211f', 'rule_name': 'Binary Masquerading via Untrusted Path'},
+        {'rule_id': '5db08297-bf72-49f4-b426-f405c2b01326', 'rule_name': 'Regsvr32 with Unusual Arguments'}
+    ],
     siem=[],
     techniques=['T1218', 'T1218.007'],
 )
