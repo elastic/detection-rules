45400d7689a354112eaa28aaffe8df617252e69b
Mika Ayenson <mika.ayenson@elastic.co>
2023-03-22 17:03:32 -0400

    add more rtas and update ditto bins

--- rta/pkg_install_chmod.py
+++ rta/pkg_install_chmod.py
@@ -14,9 +14,10 @@ metadata = RtaMetadata(
     endpoint=[
         {"rule_name": "File Made Executable via Pkg Install Script", "rule_id": "75f5d51a-218f-4d5b-80e5-eb74e498fde4"},
         {"rule_name": "File Made Executable by Suspicious Parent Process", "rule_id": "42ab2c0f-b10d-467d-8c6d-def890cf3f68"},
+        {"rule_name": "Suspicious File Create via Pkg Install Script", "rule_id": "f06d9987-33f8-44b7-b815-c1f66fb39d25"},
     ],
     siem=[],
-    techniques=["T1222", "T1222.002"],
+    techniques=["T1222", "T1222.002","T1564", "T1546", "T1546.016"],
 )
 
 
@@ -24,17 +25,22 @@ metadata = RtaMetadata(
 def main():
 
 
+    dest_file = "/tmp/test.py"
+    source_file = "/tmp/test.txt"
     masquerade = "/Users/bash"
     common.create_macos_masquerade(masquerade)
 
     # Execute command
-    command = "chmod +x /tmp/test.txt"
+    command = f"chmod +x {source_file}"
     common.log("Launching fake bash commands to execute chmod on file via pkg install")
-    with common.temporary_file("testing", "/tmp/test.txt"):
-        common.execute([masquerade, "childprocess", command, "/tmp/PKInstallSandbox.*/Scripts/*/postinstall"], timeout=10, kill=True)
+    with common.temporary_file("testing", source_file):
+        common.execute([masquerade, "childprocess", command, "childprocess",
+                        f"cp {source_file} {dest_file}", "childprocess", "/tmp/PKInstallSandbox.*/Scripts/*/postinstall"],
+                       timeout=10, kill=True)
 
     # cleanup
     common.remove_file(masquerade)
+    common.remove_file(dest_file)
 
 
 if __name__ == "__main__":
