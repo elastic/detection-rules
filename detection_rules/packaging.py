ccf8de6b3916e390d1dd3232c867633ad7504512
Terrance DeJesus <99630311+terrancedejesus@users.noreply.github.com>
2023-04-21 11:03:29 -0400

    [FR] Generate Historical Rule Files in Build Release Packages (#2715)
    * adding solution for historical rules in release package
    
    * addressing flake errors
    
    * format changes
    
    * REVERT CHANGES - testing release-fleet workflow
    
    * REVERTING CHANGES
    
    * added historical flag for packaging to account for older branches
    
    * addressing flake errors
    
    * updated build for CI
    
    * REMOVE: This is temporary to run a workflow from this branch
    
    * updates to address requirements for contents
    
    * reverting packages.yml
    
    * Update detection_rules/integrations.py
    
    Co-authored-by: Mika Ayenson <Mikaayenson@users.noreply.github.com>
    
    * Update detection_rules/integrations.py
    
    Co-authored-by: Mika Ayenson <Mikaayenson@users.noreply.github.com>
    
    * addressed feedback and added click echo comments
    
    * addressed flake errors and added some comments
    
    ---------
    
    Co-authored-by: Mika Ayenson <Mikaayenson@users.noreply.github.com>
    
    (cherry picked from commit b5ef2f5f02fbeb6d73c74bd1cecb102c1264725c)

--- detection_rules/packaging.py
+++ detection_rules/packaging.py
@@ -77,7 +77,7 @@ class Package(object):
     def __init__(self, rules: RuleCollection, name: str, release: Optional[bool] = False,
                  min_version: Optional[int] = None, max_version: Optional[int] = None,
                  registry_data: Optional[dict] = None, verbose: Optional[bool] = True,
-                 generate_navigator: bool = False):
+                 generate_navigator: bool = False, historical: bool = False):
         """Initialize a package."""
         self.name = name
         self.rules = rules
@@ -85,6 +85,7 @@ class Package(object):
         self.release = release
         self.registry_data = registry_data or {}
         self.generate_navigator = generate_navigator
+        self.historical = historical
 
         if min_version is not None:
             self.rules = self.rules.filter(lambda r: min_version <= r.contents.latest_version)
@@ -221,7 +222,7 @@ class Package(object):
         return sha256
 
     @classmethod
-    def from_config(cls, config: dict = None, verbose: bool = False) -> 'Package':
+    def from_config(cls, config: dict = None, verbose: bool = False, historical: bool = False) -> 'Package':
         """Load a rules package given a config."""
         all_rules = RuleCollection.default()
         config = config or {}
@@ -238,7 +239,7 @@ class Package(object):
         if verbose:
             click.echo(f' - {len(all_rules) - len(rules)} rules excluded from package')
 
-        package = cls(rules, verbose=verbose, **config)
+        package = cls(rules, verbose=verbose, historical=historical, **config)
 
         return package
 
@@ -399,8 +400,16 @@ class Package(object):
         # shutil.copyfile(CHANGELOG_FILE, str(rules_dir.joinpath('CHANGELOG.json')))
 
         for rule in self.rules:
-            asset_path = rules_dir / f'{rule.id}.json'
-            asset_path.write_text(json.dumps(rule.get_asset(), indent=4, sort_keys=True), encoding="utf-8")
+            asset = rule.get_asset()
+            if self.historical:
+                # if this package includes historical rules the IDs need to be changed
+                # asset['id] and the file name needs to resemble RULEID_VERSION instead of RULEID
+                asset_id = f"{asset['attributes']['rule_id']}_{asset['attributes']['version']}"
+                asset["id"] = asset_id
+                asset_path = rules_dir / f'{asset_id}.json'
+            else:
+                asset_path = rules_dir / f'{asset["id"]}.json'
+            asset_path.write_text(json.dumps(asset, indent=4, sort_keys=True), encoding="utf-8")
 
         notice_contents = Path(NOTICE_FILE).read_text()
         readme_text = textwrap.dedent("""
@@ -469,6 +478,33 @@ class Package(object):
             importable_rules_docs.append(rule_doc)
 
         return bulk_upload_docs, importable_rules_docs
+
+    @staticmethod
+    def add_historical_rules(historical_rules: Dict[str, dict], manifest_version: str) -> list:
+        """Adds historical rules to existing build package."""
+        rules_dir = CURRENT_RELEASE_PATH / 'fleet' / manifest_version / 'kibana' / 'security_rule'
+
+        # iterates over historical rules from previous package and writes them to disk
+        for historical_rule_id, historical_rule_contents in historical_rules.items():
+            rule_id = historical_rule_contents["attributes"]["rule_id"]
+            historical_rule_version = historical_rule_contents['attributes']['version']
+
+            # checks if the rule exists in the current package first
+            current_rule_path = list(rules_dir.glob(f"{rule_id}*.json"))
+            if not current_rule_path:
+                continue
+
+            # load the current rule from disk
+            current_rule_path = current_rule_path[0]
+            current_rule_json = json.load(current_rule_path.open(encoding="UTF-8"))
+            current_rule_version = current_rule_json['attributes']['version']
+
+            # if the historical rule version and current rules version differ, write
+            # the historical rule to disk
+            if historical_rule_version != current_rule_version:
+                historical_rule_path = rules_dir / f"{historical_rule_id}.json"
+                with historical_rule_path.open("w", encoding="UTF-8") as file:
+                    json.dump(historical_rule_contents, file)
 
 
 @cached
