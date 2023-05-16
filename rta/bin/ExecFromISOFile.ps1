725aa5ceb993d7851cc3e43babfbf5c5b822b7b4
Samirbous <64742097+Samirbous@users.noreply.github.com>
2023-05-16 08:30:03 +0100

    Update ExecFromISOFile.ps1

--- rta/bin/ExecFromISOFile.ps1
+++ rta/bin/ExecFromISOFile.ps1
@@ -6,11 +6,11 @@ function ExecFromISO {
 		[string] $procname,
 		[string] $cmdline
 	)
-	$MountMeta = Mount-DiskImage -ImagePath $ISOFile -StorageType ISO -Access ReadOnly; 
-	$DriveLetter = ($MountMeta | Get-Volume).DriveLetter; 
+	$MountMeta = Mount-DiskImage -ImagePath $ISOFile -StorageType ISO -Access ReadOnly
+	$DriveLetter = ($MountMeta | Get-Volume).DriveLetter
 	if ($cmdline) {Start-Process -FilePath "$($DriveLetter):\$($procname)" -ArgumentList "$($cmdline)";}
-	else {invoke-item "$($DriveLetter):\$($procname)";} 
-	Start-Sleep -s 2; 
-	Stop-process -name $procname -Force -ErrorAction ignore; 
-	Dismount-DiskImage -ImagePath $ISOFile | Out-Null;
+	else {invoke-item "$($DriveLetter):\$($procname)"} 
+	Start-Sleep -s 2
+	Stop-process -name $procname -Force -ErrorAction ignore
+	Dismount-DiskImage -ImagePath $ISOFile | Out-Null
 }
