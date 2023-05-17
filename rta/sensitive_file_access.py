5f570e6d0f83a00f6f64f253265eca38d12c5f13
Samirbous <64742097+Samirbous@users.noreply.github.com>
2023-05-17 12:42:28 +0100

    added ntds.dit access

--- rta/sensitive_file_access.py
+++ rta/sensitive_file_access.py
@@ -18,6 +18,7 @@ metadata = RtaMetadata(
         {"rule_name": "Sensitive File Access - Unattended Panther", "rule_id": "52e4ad92-e09b-4331-b827-cd0f2cbaf576"},
         {"rule_name": "Potential Discovery of Windows Credential Manager Store", "rule_id": "cc60be0e-2c6c-4dc9-9902-e97103ff8df9"},
         {"rule_name": "Potential Discovery of DPAPI Master Keys", "rule_id": "84bbe951-5141-4eb3-b9cf-8dfeea62a94e"},
+        {"rule_name": "Suspicious Access to Active Directory Database File", "rule_id": "d66765b8-010b-4a40-ab62-1d8f13a44878"},
     ],    
     siem=[],
     techniques=["T1134"],
@@ -36,7 +37,8 @@ def main():
              "C:\\Users\\Public\\AppData\\Local\\Microsoft\\Vault\\test",
              "%appdata%\\Microsoft\\Credentials\\test",
              "C:\\Windows\\Panther\\Unattend.xml",
-             "C:\\Windows\\System32\\Microsoft\\Protect\\S-1-5-18\\User\\test"]
+             "C:\\Windows\\System32\\Microsoft\\Protect\\S-1-5-18\\User\\test",
+             "C:\\Windows\\NTDS\\NTDS.dit"]
     for f in files:
         try:
             win32file.CreateFile(path.expandvars(f), win32file.GENERIC_READ, 0, None, OPEN_EXISTING, 0, None)
