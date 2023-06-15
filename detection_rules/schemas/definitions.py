bb0dd64eee03638d2e3f2d56f589ccd46269cfe1
eric-forte-elastic <119343520+eric-forte-elastic@users.noreply.github.com>
2023-06-15 13:17:26 -0400

    Update detection_rules/schemas/definitions.py
    Co-authored-by: Justin Ibarra <16747370+brokensound77@users.noreply.github.com>

--- detection_rules/schemas/definitions.py
+++ detection_rules/schemas/definitions.py
@@ -35,7 +35,6 @@ SUBTECHNIQUE_URL = r'^https://attack.mitre.org/techniques/T[0-9]+/[0-9]+/$'
 MACHINE_LEARNING = 'machine_learning'
 SAVED_QUERY = 'saved_query'
 QUERY = 'query'
-BUILDING_BLOCK_TYPE = 'default'
 
 # we had a bad rule ID make it in before tightening up the pattern, and so we have to let it bypass
 KNOWN_BAD_RULE_IDS = Literal['119c8877-8613-416d-a98a-96b6664ee73a5']
