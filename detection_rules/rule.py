4694dcbd148b332bb378c501f5de9af48ec973ec
eric-forte-elastic <eric.forte@elastic.co>
2023-06-15 15:21:28 -0400

    fixed error message

--- detection_rules/rule.py
+++ detection_rules/rule.py
@@ -431,11 +431,13 @@ class DataValidator:
         if self.building_block_type:
             if not self.from_ or not self.interval:
                 raise ValidationError(
+                    f"{self.name} is invalid."
                     "BBR require `from` and `interval` to be defined. Please set or bypass."
                     "To bypass, use the environment variable `DR_BYPASS_BBR_LOOKBACK_VALIDATION`"
                 )
             elif not validate_time_defaults(self.from_) or not validate_time_defaults(self.interval):
                 raise ValidationError(
+                    f"{self.name} is invalid."
                     "Default BBR require `from` and `interval` to be at least now-119m and at least 60m respectively "
                     "(using the now-Xm and Xm format where x is in minuets). Please update values or bypass. "
                     "To bypass, use the environment variable `DR_BYPASS_BBR_LOOKBACK_VALIDATION`"
