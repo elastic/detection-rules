330d0250d512f7263563fc5f9cd3c8d710a0e1d9
Samirbous <64742097+Samirbous@users.noreply.github.com>
2023-06-23 10:00:44 +0100

    Update evasion_ntdll_from_unusual_path.py

--- rta/evasion_ntdll_from_unusual_path.py
+++ rta/evasion_ntdll_from_unusual_path.py
@@ -6,8 +6,7 @@
 
 from . import common
 from . import RtaMetadata
-import win32file, win32api, os
-from os import path
+
 
 metadata = RtaMetadata(
     uuid="e6d5315f-4c70-4788-8564-e7c23786a4d0",
@@ -17,19 +16,22 @@ metadata = RtaMetadata(
     techniques=["T1055"],
 )
 
+
+
 @common.requires_os(metadata.platforms)
-
-
-def main(): 
+def main():
+    import win32file, win32api, os, time
+    from os import path
     win32file.CopyFile(path.expandvars("%systemroot%\\system32\\ntdll.dll"), path.expandvars("%localappdata%\\Temp\\notntdll.dll"), 0) 
-    if os.path.exists("c:\\users\\public\\notntdll.dll"): 
+    if os.path.exists(path.expandvars("%localappdata%\\Temp\\notntdll.dll")):
        print(f"[+] - NTDLL copied")
        r = win32api.LoadLibrary(path.expandvars("%localappdata%\\Temp\\notntdll.dll")) 
        if r > 0 : 
           print(f"[+] - NTDLL copy loaded")
+          time.sleep(1)
           win32api.FreeLibrary(r)
           win32file.DeleteFile(path.expandvars("%localappdata%\\Temp\\notntdll.dll"))
-          print('f[+] - NTDLL copy deleted')
+          print(f'[+] - NTDLL copy deleted')
        else :  
           print('f[+] - Failed to load ntdll')
          
