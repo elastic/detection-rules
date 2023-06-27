8dedfca9aacda8d72bacb5565af1669fd9c29b3b
Mika Ayenson <Mika.ayenson@elastic.co>
2023-06-27 17:44:53 -0400

    add method to build a limited compatible rule ndjson for older stacks

--- detection_rules/main.py
+++ detection_rules/main.py
@@ -11,23 +11,25 @@ import os
 import re
 import time
 from datetime import datetime
+from marshmallow_dataclass import class_schema
 from pathlib import Path
-from typing import Dict, Optional
+from semver import Version
+from typing import Dict, List, Optional
 from uuid import uuid4
 
 import click
 
-
 from .cli_utils import rule_prompt, multi_collection
 from .mappings import build_coverage_map, get_triggered_rules, print_converage_summary
 from .misc import add_client, client_error, nested_set, parse_config, load_current_package_version
-from .rule import TOMLRule, TOMLRuleContents
+from .rule import TOMLRule, TOMLRuleContents, QueryRuleData
 from .rule_formatter import toml_write
 from .rule_loader import RuleCollection
-from .schemas import all_versions, definitions
+from .schemas import all_versions, definitions, get_incompatible_fields
 from .utils import get_path, get_etc_path, clear_caches, load_dump, load_rule_contents
 
 RULES_DIR = get_path('rules')
+UNSUPPORTED_RULE_TYPES = {'new_terms', 'threat_match', 'threshold', 'machine_learning'}
 
 
 @click.group('detection-rules', context_settings={'help_option_names': ['-h', '--help']})
@@ -111,6 +113,54 @@ def import_rules(input_file, directory):
         base_path = name_to_filename(base_path) if base_path else base_path
         rule_path = os.path.join(RULES_DIR, base_path) if base_path else None
         rule_prompt(rule_path, required_only=True, save=True, verbose=True, additional_required=['index'], **contents)
+
+
+@root.command('build-limited-rules')
+@click.option('--stack-version', type=click.Choice(all_versions()), required=True,
+              help='Version to downgrade to be compatible with the older instance of Kibana')
+@click.option('--output-file', '-o', type=click.Path(dir_okay=False, exists=False), required=True)
+def build_limited_rules(stack_version: str, output_file: str):
+    """
+    Import rules from json, toml, or Kibana exported rule file(s),
+    filter out unsupported ones, and write to output NDJSON file.
+    """
+
+    # Schema generation and incompatible fields detection
+    query_rule_data = class_schema(QueryRuleData)()
+    fields = getattr(query_rule_data, 'fields', {})
+    incompatible_fields = get_incompatible_fields(list(fields.values()),
+                                                  Version.parse(stack_version, optional_minor_and_patch=True))
+
+    # Load all rules
+    rules = RuleCollection.default()
+
+    # Define output path
+    output_path = Path(output_file)
+
+    # Function to process and write each rule to the output file
+    def process_and_write_rule(rule, incompatible_fields: List[str], file):
+        if rule.contents.type in UNSUPPORTED_RULE_TYPES:
+            click.secho(f'Skipping unsupported rule type: {rule.contents.get("type")}', fg='yellow')
+            return
+
+        # Remove unsupported fields from rule
+        rule_contents = rule.contents.to_api_format()
+        for field in incompatible_fields:
+            rule_contents.pop(field, None)
+
+        # Try to write the cleaned rule to the NDJSON output file
+        try:
+            json.dump(rule_contents, file)
+            file.write('\n')
+        except json.JSONDecodeError:
+            click.echo(f"Error: Failed to write rule {rule_contents.get('name')} to the file.", err=True)
+
+    # Process and write each rule
+    with output_path.open('w', encoding="utf-8") as out_file:
+        for rule in rules.rules:
+            process_and_write_rule(rule, incompatible_fields, out_file)
+
+    click.echo(f'Success: Rules written to {output_file}')
 
 
 @root.command('toml-lint')
