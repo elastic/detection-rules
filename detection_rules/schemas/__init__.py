3f9e7aced1aefa56cb5b0d4c16fd040aabe4fd7f
Mika Ayenson <Mikaayenson@users.noreply.github.com>
2023-08-02 12:38:48 -0500

    [Bug] Strip Non-Public Fields Prior to Uploading Rules (#2986)

--- detection_rules/schemas/__init__.py
+++ detection_rules/schemas/__init__.py
@@ -65,9 +65,6 @@ def get_schema_file(version: Version, rule_type: str) -> dict:
 def strip_additional_properties(version: Version, api_contents: dict) -> dict:
     """Remove all fields that the target schema doesn't recognize."""
 
-    if Version.parse(version, optional_minor_and_patch=True) >= Version.parse("8.3.0"):
-        api_contents = strip_build_time_fields(api_contents)
-
     stripped = {}
     target_schema = get_schema_file(version, api_contents["type"])
 
@@ -80,14 +77,13 @@ def strip_additional_properties(version: Version, api_contents: dict) -> dict:
     return stripped
 
 
-def strip_build_time_fields(api_contents: dict) -> dict:
-    """Remove all fields that are only used at build time."""
-    contents = api_contents.copy()
-    if "related_integrations" in contents:
-        del contents["related_integrations"]
-    if "required_fields" in contents:
-        del contents["required_fields"]
-    return contents
+def strip_non_public_fields(min_stack_version: Version, data_dict: dict) -> dict:
+    """Remove all non public fields."""
+    for field, version_range in definitions.NON_PUBLIC_FIELDS.items():
+        if version_range[0] <= min_stack_version <= (version_range[1] or min_stack_version):
+            if field in data_dict:
+                del data_dict[field]
+    return data_dict
 
 
 @migrate("7.8")
