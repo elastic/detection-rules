d65c151960d1e396f921a485c31cb71bacb4728c
Jonhnathan <26856693+w0rk3r@users.noreply.github.com>
2023-10-18 09:15:19 -0300

    Revertable merge

--- detection_rules/schemas/definitions.py
+++ detection_rules/schemas/definitions.py
@@ -90,6 +90,7 @@ EXPECTED_RULE_TAGS = [
     'OS: Linux',
     'OS: macOS',
     'OS: Windows',
+    'Rule Type: BBR',
     'Resources: Investigation Guide',
     'Rule Type: Higher-Order Rule',
     'Rule Type: Machine Learning',
