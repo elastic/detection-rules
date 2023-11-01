117e7d619b7953bd155b51fdb61b52b2eb36f2ae
terrancedejesus <terrance.dejesus@elastic.co>
2023-11-01 10:27:11 -0400

    added sub-dataclasses for V3

--- detection_rules/schemas/registry_package.py
+++ detection_rules/schemas/registry_package.py
@@ -5,12 +5,27 @@
 
 """Definitions for packages destined for the registry."""
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Dict, List, Optional
 
 from .definitions import ConditionSemVer, SemVer
 from ..mixins import MarshmallowDataclassMixin
+from marshmallow_dataclass import field_for_schema
 
+@dataclass
+class ConditionElastic:
+    subscription: str
+
+@dataclass
+class Condition:
+    kibana_version: str = field(metadata={"data_key": "kibana.version"})
+    elastic: ConditionElastic
+
+@dataclass
+class Icon:
+    size: str
+    src: str
+    type: str
 
 @dataclass
 class RegistryPackageManifest(MarshmallowDataclassMixin):
@@ -20,7 +35,7 @@ class RegistryPackageManifest(MarshmallowDataclassMixin):
     conditions: Dict[str, ConditionSemVer]
     description: str
     format_version: SemVer
-    icons: list
+    icons: List[Icon]
     license: str
     name: str
     owner: Dict[str, str]
@@ -39,10 +54,10 @@ class RegistryPackageManifestV3(MarshmallowDataclassMixin):
     """Base class for registry packages using elastic-package v3."""
 
     categories: List[str]
-    conditions: Dict[str, ConditionSemVer]
+    conditions: Condition
     description: str
     format_version: SemVer
-    icons: list
+    icons: List[Icon]
     source: Dict[str, str]
     name: str
     owner: Dict[str, str]
@@ -51,5 +66,5 @@ class RegistryPackageManifestV3(MarshmallowDataclassMixin):
     version: SemVer
 
     internal: Optional[bool] = None
-    policy_templates: Optional[list] = None
-    screenshots: Optional[list] = None
+    policy_templates: Optional[List[str]] = None
+    screenshots: Optional[List[str]] = None
