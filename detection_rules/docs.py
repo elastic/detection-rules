a08c9ef05912e8291116696367ebee3cbac278d3
terrancedejesus <terrance.dejesus@elastic.co>
2024-02-22 21:19:43 -0500

    added content() class method for guide and setup

--- detection_rules/docs.py
+++ detection_rules/docs.py
@@ -250,6 +250,10 @@ class AsciiDoc:
         return f'[source, {code}]\n{line_sep}\n{value}\n{line_sep}'
 
     @classmethod
+    def content(cls, value: str):
+        return f'{value}'
+
+    @classmethod
     def title(cls, depth: int, value: str):
         return f'{"=" * depth} {value}'
 
@@ -594,12 +598,12 @@ class IntegrationRuleDetail:
     def guide_str(self) -> str:
         """Add the guide section to the rule detail page."""
         guide = convert_markdown_urls_to_asciidoc(self.rule['note'])
-        return f'{AsciiDoc.title(4, "Investigation guide")}\n\n\n{AsciiDoc.code(guide, code="markdown")}'
+        return f'{AsciiDoc.title(4, "Investigation guide")}\n\n\n{AsciiDoc.content(guide)}'
 
     def setup_str(self) -> str:
         """Add the setup section to the rule detail page."""
         setup = convert_markdown_urls_to_asciidoc(self.rule['setup'])
-        return f'{AsciiDoc.title(4, "Setup")}\n\n\n{AsciiDoc.code(setup, code="markdown")}'
+        return f'{AsciiDoc.title(4, "Setup")}\n\n\n{AsciiDoc.content(setup)}'
 
     def query_str(self) -> str:
         """Add the query section to the rule detail page."""
