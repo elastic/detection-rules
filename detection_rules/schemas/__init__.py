61bc90359fc7f951901ebb3f5930b46c48ef0b11
shashank-elastic <91139415+shashank-elastic@users.noreply.github.com>
2024-03-06 21:26:54 +0530

    Prepare For Next Elastic Stack Minor Release (#3490)
    Removed changes from:
    - detection_rules/etc/packages.yml
    
    (selectively cherry picked from commit a4094df7320f9b699be63a229454cfbb1cbe51bd)

--- detection_rules/schemas/__init__.py
+++ detection_rules/schemas/__init__.py
@@ -274,6 +274,12 @@ def migrate_to_8_12(version: Version, api_contents: dict) -> dict:
     return strip_additional_properties(version, api_contents)
 
 
+@migrate("8.13")
+def migrate_to_8_13(version: Version, api_contents: dict) -> dict:
+    """Default migration for 8.13."""
+    return strip_additional_properties(version, api_contents)
+
+
 def downgrade(api_contents: dict, target_version: str, current_version: Optional[str] = None) -> dict:
     """Downgrade a rule to a target stack version."""
     from ..packaging import current_stack_version
