afb974a2f16aa91136f907f46f5b16ca830536a7
Justin Ibarra <16747370+brokensound77@users.noreply.github.com>
2024-04-26 11:12:50 -0600

    Refresh Kibana module with API updates (#3466)
    * Refresh Kibana module with API updates
    * add import/export commands
    * rename repo commands
    * add RawRuleCollection and DictRule objects
    * save exported rules to files; rule.from_rule_resource
    * strip unknown fields in schema
    * add remote cli test
    * update docs
    * bump kibana lib version
    
    ---------
    
    Co-authored-by: brokensound77 <brokensound77@users.noreply.github.com>
    
    (cherry picked from commit c567d3731af91ddd7616c1ba008d427da6868da6)

--- detection_rules/cli_utils.py
+++ detection_rules/cli_utils.py
@@ -50,7 +50,7 @@ def single_collection(f):
 
         if rule_id:
             rules.load_directories((DEFAULT_RULES_DIR, DEFAULT_BBR_DIR),
-                                   toml_filter=dict_filter(rule__rule_id=rule_id))
+                                   obj_filter=dict_filter(rule__rule_id=rule_id))
             if len(rules) != 1:
                 client_error(f"Could not find rule with ID {rule_id}")
 
@@ -66,7 +66,7 @@ def multi_collection(f):
 
     @click.option('--rule-file', '-f', multiple=True, type=click.Path(dir_okay=False), required=False)
     @click.option('--directory', '-d', multiple=True, type=click.Path(file_okay=False), required=False,
-                  help='Recursively export rules from a directory')
+                  help='Recursively load rules from a directory')
     @click.option('--rule-id', '-id', multiple=True, required=False)
     @functools.wraps(f)
     def get_collection(*args, **kwargs):
@@ -84,7 +84,7 @@ def multi_collection(f):
 
         if rule_id:
             rules.load_directories((DEFAULT_RULES_DIR, DEFAULT_BBR_DIR),
-                                   toml_filter=dict_filter(rule__rule_id=rule_id))
+                                   obj_filter=dict_filter(rule__rule_id=rule_id))
             found_ids = {rule.id for rule in rules}
             missing = set(rule_id).difference(found_ids)
 
