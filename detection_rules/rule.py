213450169bd664d2615c4d302dc6f48489e64dab
brokensound77 <brokensound77@users.noreply.github.com>
2024-05-01 11:27:52 -0600

    Update query with tls.client.server_name; update check for esql instance

--- detection_rules/rule.py
+++ detection_rules/rule.py
@@ -597,7 +597,7 @@ class QueryValidator:
     @cached
     def get_required_fields(self, index: str) -> List[Optional[dict]]:
         """Retrieves fields needed for the query along with type information from the schema."""
-        if not self.ast:
+        if isinstance(self, ESQLValidator):
             return []
 
         current_version = Version.parse(load_current_package_version(), optional_minor_and_patch=True)
