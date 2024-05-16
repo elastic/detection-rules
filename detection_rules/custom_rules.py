20c4c56cfb697fac856f73c39410f124a7e533af
eric-forte-elastic <eric.forte@elastic.co>
2024-05-15 20:52:42 -0400

    update to yaml

--- detection_rules/custom_rules.py
+++ detection_rules/custom_rules.py
@@ -57,7 +57,7 @@ def create_test_config_content() -> str:
 
 @custom_rules.command('setup-config')
 @click.argument('directory', type=Path)
-@click.argument('kibana-version', type=str, default=load_etc_dump('packages.yml')['package']['name'])
+@click.argument('kibana-version', type=str, default=load_etc_dump('packages.yaml')['package']['name'])
 @click.option('--overwrite', is_flag=True, help="Overwrite the existing _config.yaml file.")
 def setup_config(directory: Path, kibana_version: str, overwrite: bool):
     """Setup the custom rules configuration directory and files with defaults."""
