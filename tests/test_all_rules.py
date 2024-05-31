e28ce4911979db764fb0f1adb8e3fc4325c64f00
Shashank K S <Shashank.Suryanarayana@elastic.co>
2024-05-31 22:12:37 +0530

    Add origin branch referecne

--- tests/test_all_rules.py
+++ tests/test_all_rules.py
@@ -1295,7 +1295,7 @@ class TestDeprecatedRule(BaseRuleTest):
         # Iterate over all the files in the deprecated rules directory
         for rule in self.deprecated_rules:
             # Use git diff to check if the file has changed
-            result = subprocess.run(['git', 'diff', 'main', '--name-only', rule.path], stdout=subprocess.PIPE)
+            result = subprocess.run(['git', 'diff', 'origin/main', '--name-only', rule.path], stdout=subprocess.PIPE)
 
             # If the output is not empty, the file has changed
             if result.stdout:
