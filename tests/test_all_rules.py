0ba48d3cd7a2914682f4feaefaa030384e4312b4
Shashank K S <Shashank.Suryanarayana@elastic.co>
2024-06-13 19:13:34 +0530

    Lint Errors

--- tests/test_all_rules.py
+++ tests/test_all_rules.py
@@ -665,7 +665,8 @@ class TestRuleMetadata(BaseRuleTest):
                 last_commit_date = detection_rules_git('log', '-1', '--format=%cd', '--date=short', rule_path)
                 if rule.contents.metadata.updated_date < last_commit_date.replace('-', '/'):
                     # Rule has been modified but updated_date has not been changed, add to list
-                    warn_msg = f'{self.rule_str(rule)} Rule has updated_date {rule.contents.metadata.updated_date} but modified on {last_commit_date.replace('-', '/')}'
+                    warn_msg = f'Rule {self.rule_str(rule)} has updated_date {rule.contents.metadata.updated_date} \
+                        but modified on {last_commit_date.replace('-', '/')}'
                     failed_rules.append(warn_msg)
 
             if failed_rules:
