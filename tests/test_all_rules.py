5a12def958ef26319182bde13c9e952c528f3b2a
Shashank K S <Shashank.Suryanarayana@elastic.co>
2024-06-17 18:22:14 +0530

    Lint errors

--- tests/test_all_rules.py
+++ tests/test_all_rules.py
@@ -678,7 +678,7 @@ class TestRuleMetadata(BaseRuleTest):
                             err_msg = f'{self.rule_str(rule)} {rule_integration} tag, path is {rule.path.parent.name}'
                             failures.append(err_msg)
 
-                    # checks if an index pattern exists if the package integration tag exists 
+                    # checks if an index pattern exists if the package integration tag exists
                     # and is of pattern logs-{integration}*
                     integration_string = "|".join(indices)
                     if not re.search(f"logs-{rule_integration}*", integration_string):
@@ -686,7 +686,8 @@ class TestRuleMetadata(BaseRuleTest):
                                 any(ri in [*map(str.lower, definitions.MACHINE_LEARNING_PACKAGES)]
                                     for ri in rule_integrations):
                             continue
-                        elif rule_integration == "apm" and re.search("apm-*-transaction*|traces-apm*", integration_string):
+                        elif rule_integration == "apm" and \
+                                re.search("apm-*-transaction*|traces-apm*", integration_string):
                             continue
                         elif rule.contents.data.type == 'threat_match':
                             continue
