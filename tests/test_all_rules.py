0de6014463b7b7a13609dc151d5e39f378d56ac6
Jonhnathan <26856693+w0rk3r@users.noreply.github.com>
2024-06-25 12:11:06 -0300

    .

--- tests/test_all_rules.py
+++ tests/test_all_rules.py
@@ -311,7 +311,8 @@ class TestRuleTags(BaseRuleTest):
             'logs-windows.sysmon_operational-*': {'all': ['Data Source: Sysmon']},
             'logs-windows.powershell*': {'all': ['Data Source: PowerShell Logs']},
             'logs-sentinel_one_cloud_funnel.*': {'all': ['Data Source: SentinelOne']},
-            'logs-fim.event-*': {'all': ['Data Source: File Integrity Monitoring']}
+            'logs-fim.event-*': {'all': ['Data Source: File Integrity Monitoring']},
+            'logs-m365_defender.event-*': {'all': ['Data Source: Microsoft Defender for Endpoint']}
         }
 
         for rule in self.all_rules:
