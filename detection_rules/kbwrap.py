6885c57107994dde99caccdec9fdd117341e23c8
eric-forte-elastic <eric.forte@elastic.co>
2024-07-07 13:52:10 -0400

    Switch to Schema Identification

--- detection_rules/kbwrap.py
+++ detection_rules/kbwrap.py
@@ -9,13 +9,16 @@ from pathlib import Path
 from typing import Iterable, List, Optional
 
 import click
+from marshmallow import EXCLUDE, ValidationError
+from marshmallow_dataclass import class_schema
 
 import kql
 from kibana import Signal, RuleResource
 
 from .config import parse_rules_config
 from .cli_utils import multi_collection
-from .exception import TOMLException, TOMLExceptionContents
+from .exception import (DetectionException, ExceptionContainer, TOMLException,
+                        TOMLExceptionContents)
 from .main import root
 from .misc import add_params, client_error, kibana_options, get_kibana_client, nested_set
 from .rule import downgrade_contents_from_rule, TOMLRuleContents, TOMLRule
@@ -172,25 +175,31 @@ def kibana_export_rules(
     # Parse exceptions results from API return
     exceptions = []
     if export_exceptions:
-        try:
-            # API returns a list of results,
-            # some are exception list containers and some are exception list items
-            exceptions_containers = {
-                res.get("list_id"): res
-                for res in exception_results
-                if "Exception list containing exceptions" in res.get("description")
-            }
-            exceptions_items = {list_id: [] for list_id in exceptions_containers.keys()}
+        exceptions_containers = {}
+        exceptions_items = {}
 
-            for res in exception_results:
-                if res.get("description") == "Exception list item":
+        # Create schemas for your dataclasses
+        ExceptionContainerSchema = class_schema(ExceptionContainer)()  # noqa F821
+        DetectionExceptionSchema = class_schema(DetectionException)()  # noqa F821
+
+        for res in exception_results:
+            try:
+                # Try to load the data into the ExceptionContainer schema
+                ExceptionContainerSchema.load(res, unknown=EXCLUDE)
+                exceptions_containers[res.get("list_id")] = res
+            except ValidationError:
+                try:
+                    # Try to load the data into the DetectionException schema
+                    DetectionExceptionSchema.load(res, unknown=EXCLUDE)
                     list_id = res.get("list_id")
+                    if list_id not in exceptions_items:
+                        exceptions_items[list_id] = []
                     exceptions_items[list_id].append(res)
-        except Exception as e:
-            if skip_errors:
-                print(f"- skipping exceptions export unable to parse API result - {type(e).__name__}")
-                errors.append(f"- exceptions export - {e}")
-            raise
+                except Exception as e:
+                    if skip_errors:
+                        print(f"- skipping exceptions export unable to parse API result - {type(e).__name__}")
+                        errors.append(f"- exceptions export - {e}")
+                    raise
 
         # Build TOMLException Objects
         for container in exceptions_containers.values():
