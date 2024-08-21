6378b00be227624690e5a9491078cdf6c4004358
Eric Forte <119343520+eric-forte-elastic@users.noreply.github.com>
2024-08-21 18:00:02 -0400

    [Bug] Add historical Rules as Default when Build Package (#4003)
    * Add historical Rules as Default
    
    * Update num latest rule versions
    
    * Update split for parsing
    
    * Update saved version
    
    * Remove if else
    
    * write historical rules with versions
    
    ---------
    
    Co-authored-by: Mika Ayenson <Mika.ayenson@elastic.co>
    
    (cherry picked from commit 589aa335084307f9024c8e8b6309238e47bb5457)

--- detection_rules/packaging.py
+++ detection_rules/packaging.py
@@ -228,7 +228,7 @@ class Package(object):
 
     @classmethod
     def from_config(cls, rule_collection: Optional[RuleCollection] = None, config: Optional[dict] = None,
-                    verbose: Optional[bool] = False) -> 'Package':
+                    verbose: Optional[bool] = False, historical: Optional[bool] = True) -> 'Package':
         """Load a rules package given a config."""
         all_rules = rule_collection or RuleCollection.default()
         config = config or {}
@@ -245,7 +245,7 @@ class Package(object):
         if verbose:
             click.echo(f' - {len(all_rules) - len(rules)} rules excluded from package')
 
-        package = cls(rules, verbose=verbose, **config)
+        package = cls(rules, verbose=verbose, historical=historical, **config)
 
         return package
 
@@ -412,14 +412,12 @@ class Package(object):
 
         for rule in self.rules:
             asset = rule.get_asset()
-            if self.historical:
-                # if this package includes historical rules the IDs need to be changed
-                # asset['id] and the file name needs to resemble RULEID_VERSION instead of RULEID
-                asset_id = f"{asset['attributes']['rule_id']}_{asset['attributes']['version']}"
-                asset["id"] = asset_id
-                asset_path = rules_dir / f'{asset_id}.json'
-            else:
-                asset_path = rules_dir / f'{asset["id"]}.json'
+            # if this package includes historical rules the IDs need to be changed
+            # asset['id] and the file name needs to resemble RULEID_VERSION instead of RULEID
+            asset_id = f"{asset['attributes']['rule_id']}_{asset['attributes']['version']}"
+            asset["id"] = asset_id
+            asset_path = rules_dir / f'{asset_id}.json'
+
             asset_path.write_text(json.dumps(asset, indent=4, sort_keys=True), encoding="utf-8")
 
         notice_contents = NOTICE_FILE.read_text()
@@ -502,7 +500,7 @@ class Package(object):
         rules_dir = CURRENT_RELEASE_PATH / 'fleet' / manifest_version / 'kibana' / 'security_rule'
 
         # iterates over historical rules from previous package and writes them to disk
-        for historical_rule_id, historical_rule_contents in historical_rules.items():
+        for _, historical_rule_contents in historical_rules.items():
             rule_id = historical_rule_contents["attributes"]["rule_id"]
             historical_rule_version = historical_rule_contents['attributes']['version']
 
@@ -519,7 +517,7 @@ class Package(object):
             # if the historical rule version and current rules version differ, write
             # the historical rule to disk
             if historical_rule_version != current_rule_version:
-                historical_rule_path = rules_dir / f"{historical_rule_id}.json"
+                historical_rule_path = rules_dir / f"{rule_id}_{historical_rule_version}.json"
                 with historical_rule_path.open("w", encoding="UTF-8") as file:
                     json.dump(historical_rule_contents, file)
 
