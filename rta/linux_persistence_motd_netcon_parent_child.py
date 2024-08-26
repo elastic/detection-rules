25bb2974d2d225723c2564c2f98fd1973e2ebfe5
protections machine <72879786+protectionsmachine@users.noreply.github.com>
2024-08-27 04:27:42 +1000

    Sync RTA Linux Production Tuning  (#4014)
    (cherry picked from commit cb739fb1619ad5a1db4f9aaf250108bf32fd6280)

--- rta/linux_persistence_motd_netcon_parent_child.py
+++ rta/linux_persistence_motd_netcon_parent_child.py
@@ -3,8 +3,9 @@
 # 2.0; you may not use this file except in compliance with the Elastic License
 # 2.0.
 
-from . import common
-from . import RtaMetadata
+import sys
+
+from . import RtaMetadata, common
 
 metadata = RtaMetadata(
     uuid="a67ba2b1-cace-4cb9-9b7e-12c9ffe136cb",
@@ -12,16 +13,15 @@ metadata = RtaMetadata(
     endpoint=[
         {
             "rule_name": "Egress Network Connection by MOTD Child",
-            "rule_id": "da02d81a-d432-4cfe-8aa4-fc1a31c29c98"
-        }
+            "rule_id": "da02d81a-d432-4cfe-8aa4-fc1a31c29c98",
+        },
     ],
     techniques=["T1037", "T1059", "T1071"],
 )
 
 
 @common.requires_os(*metadata.platforms)
-def main():
-
+def main() -> None:
     # Path for the fake motd executable
     masquerade = "/etc/update-motd.d/rta"
     source = common.get_path("bin", "netcon_exec_chain.elf")
@@ -29,19 +29,19 @@ def main():
     common.log("Creating a fake motd executable..")
     common.copy_file(source, masquerade)
     common.log("Granting execute permissions...")
-    common.execute(['chmod', '+x', masquerade])
+    common.execute(["chmod", "+x", masquerade])
 
     # Execute the fake motd executable
     common.log("Executing the fake motd executable..")
     commands = [
         masquerade,
-        'chain',
-        '-h',
-        '8.8.8.8',
-        '-p',
-        '53',
-        '-c',
-        '/etc/update-motd.d/rta netcon -h 8.8.8.8 -p 53'
+        "chain",
+        "-h",
+        "8.8.8.8",
+        "-p",
+        "53",
+        "-c",
+        "/etc/update-motd.d/rta netcon -h 8.8.8.8 -p 53",
     ]
     common.execute([*commands], timeout=5, kill=True)
 
@@ -50,4 +50,4 @@ def main():
 
 
 if __name__ == "__main__":
-    exit(main())
+    sys.exit(main())
