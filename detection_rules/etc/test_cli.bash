a23ad950874e63462b6e229a92f83f7357ed0b35
Shashank K S <Shashank.Suryanarayana@elastic.co>
2024-09-10 22:04:24 +0530

    Add toml-lint to the test_cli.bash script

--- detection_rules/etc/test_cli.bash
+++ detection_rules/etc/test_cli.bash
@@ -28,6 +28,9 @@ python -m detection_rules dev schemas update-rule-data
 echo "Validating rule: execution_github_new_event_action_for_pat.toml"
 python -m detection_rules validate-rule rules_building_block/execution_github_new_event_action_for_pat.toml
 
+echo "Linting Rule: execution_github_new_event_action_for_pat.toml"
+python -m detection_rules toml-lint -f rules_building_block/execution_github_new_event_action_for_pat.toml
+
 echo "Checking licenses"
 python -m detection_rules dev license-check
 
