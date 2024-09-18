bdf9a4e4c2529b8455cb412bf7eae755843bff23
Samirbous <Samir.Bousseaden@elastic.co>
2024-09-18 19:48:04 +0100

    Update definitions.py

--- detection_rules/schemas/definitions.py
+++ detection_rules/schemas/definitions.py
@@ -78,7 +78,7 @@ NON_DATASET_PACKAGES = ['apm',
                         'windows',
                         'sentinel_one_cloud_funnel',
                         'ti_rapid7_threat_command',
-                        'm365_defender', 
+                        'm365_defender'
                         'panw']
 NON_PUBLIC_FIELDS = {
     "related_integrations": (Version.parse('8.3.0'), None),
