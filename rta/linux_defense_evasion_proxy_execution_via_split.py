8b9079c28380734a7d94a4fb1ba7d2fdd7849dd4
protectionsmachine <72879786+protectionsmachine@users.noreply.github.com>
2024-10-21 10:44:40 +0000

    Sync RTA

--- rta/linux_defense_evasion_proxy_execution_via_split.py
+++ rta/linux_defense_evasion_proxy_execution_via_split.py
@@ -4,38 +4,42 @@
 # 2.0.
 
 import sys
-
 from . import RtaMetadata, common
 
 metadata = RtaMetadata(
-    uuid="96afe4b1-d8f3-4f95-b92b-645a39508174",
+    uuid="529c5cfd-4ceb-43a1-a006-40e072e4906c",
     platforms=["linux"],
     endpoint=[
         {
-            "rule_name": "Hidden Executable Initiated Egress Network Connection",
-            "rule_id": "c14705f7-ebd3-4cf7-88b3-6bff2d832f1b",
+            "rule_name": "Potential Proxy Execution via Split",
+            "rule_id": "0c63849b-2e23-4720-9608-0a402d093d3c",
         },
     ],
-    techniques=["T1564"],
+    techniques=["T1218", "T1059"],
 )
 
 
 @common.requires_os(*metadata.platforms)
 def main() -> None:
-    common.log("Creating a fake hidden executable..")
-    masquerade = "/tmp/.evil"
-    source = common.get_path("bin", "netcon_exec_chain.elf")
+    common.log("Creating a fake executable..")
+    masquerade = "/tmp/split"
+    masquerade2 = "/tmp/sh"
+
+    source = common.get_path("bin", "linux.ditto_and_spawn")
     common.copy_file(source, masquerade)
     common.log("Granting execute permissions...")
     common.execute(["chmod", "+x", masquerade])
 
-    commands = [masquerade, "netcon", "-h", "8.8.8.8", "-p", "53"]
+    source = common.get_path("bin", "linux.ditto_and_spawn")
+    common.copy_file(source, masquerade2)
+    common.log("Granting execute permissions...")
+    common.execute(["chmod", "+x", masquerade2])
+
+    commands = [masquerade, masquerade, '--filter=foo', '-c', 'whoami']
     common.execute([*commands], timeout=5, kill=True)
-
     common.log("Cleaning...")
-
     common.remove_file(masquerade)
-
+    common.remove_file(masquerade2)
     common.log("Simulation successfull!")
 
 
