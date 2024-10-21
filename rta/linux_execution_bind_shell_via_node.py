a52acb770cb7d0c14f8b4764fb8d3491cc2983da
protectionsmachine <72879786+protectionsmachine@users.noreply.github.com>
2024-10-21 12:23:05 +0000

    Sync RTA

--- rta/linux_execution_bind_shell_via_node.py
+++ rta/linux_execution_bind_shell_via_node.py
@@ -4,38 +4,35 @@
 # 2.0.
 
 import sys
-
 from . import RtaMetadata, common
 
 metadata = RtaMetadata(
-    uuid="96afe4b1-d8f3-4f95-b92b-645a39508174",
+    uuid="1ab728b4-1c06-4be4-a834-7893f1e9a26e",
     platforms=["linux"],
     endpoint=[
         {
-            "rule_name": "Hidden Executable Initiated Egress Network Connection",
-            "rule_id": "c14705f7-ebd3-4cf7-88b3-6bff2d832f1b",
+            "rule_name": "Bind Shell via Node",
+            "rule_id": "08697d36-4c07-4f54-b177-a39e473705c0",
         },
     ],
-    techniques=["T1564"],
+    techniques=["T1059", "T1071"],
 )
 
 
 @common.requires_os(*metadata.platforms)
 def main() -> None:
-    common.log("Creating a fake hidden executable..")
-    masquerade = "/tmp/.evil"
-    source = common.get_path("bin", "netcon_exec_chain.elf")
+    common.log("Creating a fake executable..")
+    masquerade = "/tmp/node"
+
+    source = common.get_path("bin", "linux.ditto_and_spawn")
     common.copy_file(source, masquerade)
     common.log("Granting execute permissions...")
     common.execute(["chmod", "+x", masquerade])
 
-    commands = [masquerade, "netcon", "-h", "8.8.8.8", "-p", "53"]
+    commands = [masquerade, '-e', 'spawnsh', 'listen']
     common.execute([*commands], timeout=5, kill=True)
-
     common.log("Cleaning...")
-
     common.remove_file(masquerade)
-
     common.log("Simulation successfull!")
 
 
