f5a6e1152a056859597ca42529e1ad2fb33199e9
Shashank K S <Shashank.Suryanarayana@elastic.co>
2024-12-13 17:50:10 +0530

    Modularity of code

--- detection_rules/rule_validators.py
+++ detection_rules/rule_validators.py
@@ -372,9 +372,7 @@ class EQLValidator(QueryValidator):
                         # auto add the field and re-validate
                         self.auto_add_field(validation_checks["stack"], data.index_or_dataview[0])
                     else:
-                        click.echo(f"Stack Error Message: {validation_checks["stack"].error_msg}")
                         click.echo(f"Stack Error Trace: {validation_checks["stack"]}")
-                        click.echo(f"Integrations Error Message: {validation_checks["integrations"].error_msg}")
                         click.echo(f"Integrations Error Trace: {validation_checks["integrations"]}")
                         raise ValueError("Error in both stack and integrations checks")
 
