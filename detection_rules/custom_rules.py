8bd0ead50ce1b713de1dda128b281a1f3ec43bf0
Sergey Polzunov <traut@users.noreply.github.com>
2025-03-07 14:34:51 +0100

    chore: use `docs-dev` instead of `docs` dir for docs (#4522)
    * chore: use `docs-dev` instead of `docs` folder
    
    * patch version bump
    
    * Rollback an incorrect rename
    
    * Use exact docs dir in the helper comment
    
    * Revert some overeager renamings
    
    * Moving `docs` to `docs-dev`
    
    * Update Docs Paths
    
    ---------
    
    Co-authored-by: eric-forte-elastic <eric.forte@elastic.co>
    
    (cherry picked from commit 3bdda091e114e60ec285d1806575172edad94435)

--- detection_rules/custom_rules.py
+++ detection_rules/custom_rules.py
@@ -11,11 +11,12 @@ import yaml
 
 from .main import root
 from .utils import get_etc_path, load_etc_dump, ROOT_DIR
+from .docs import REPO_DOCS_DIR
 
 from semver import Version
 
 DEFAULT_CONFIG_PATH = Path(get_etc_path('_config.yaml'))
-CUSTOM_RULES_DOC_PATH = Path(ROOT_DIR).joinpath('docs', 'custom-rules.md')
+CUSTOM_RULES_DOC_PATH = Path(ROOT_DIR).joinpath(REPO_DOCS_DIR, 'custom-rules.md')
 
 
 @root.group('custom-rules')
