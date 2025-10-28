652a7d476d1972fc54ff47f6609ffedbde98b0a9
Shashank K S <Shashank.Suryanarayana@elastic.co>
2025-10-28 23:13:28 +0530

    Revert

--- tests/test_gh_workflows.py
+++ tests/test_gh_workflows.py
@@ -3,7 +3,7 @@
 # 2.0; you may not use this file except in compliance with the Elastic License
 # 2.0.
 
-"""Tests for GitHub workflow functionality. """
+"""Tests for GitHub workflow functionality."""
 
 import unittest
 
