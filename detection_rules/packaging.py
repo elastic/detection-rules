7d11758ecfdab8f129de368bf21b1d4285dfbf3f
Shashank K S <Shashank.Suryanarayana@elastic.co>
2026-05-19 21:02:46 +0530

    Fix code checks

--- detection_rules/packaging.py
+++ detection_rules/packaging.py
@@ -574,8 +574,11 @@ class Package:
 
             The detection rules package stores the prebuilt security rules for the Elastic Security [detection engine](https://www.elastic.co/docs/solutions/security/detect-and-alert).
 
-            To download the rules, refer to [Install Elastic prebuilt rules](https://www.elastic.co/docs/solutions/security/detect-and-alert/install-prebuilt-rules#load-prebuilt-rules).
-            To update the rules to ensure they detect the latest threats and techniques, refer to [Update Elastic prebuilt rules](https://www.elastic.co/docs/solutions/security/detect-and-alert/update-prebuilt-rules).
+            To download the rules, refer to [Install Elastic prebuilt rules](
+            https://www.elastic.co/docs/solutions/security/detect-and-alert/install-prebuilt-rules#load-prebuilt-rules).
+            To update the rules to ensure they detect the latest threats and techniques, refer to
+            [Update Elastic prebuilt rules](
+            https://www.elastic.co/docs/solutions/security/detect-and-alert/update-prebuilt-rules).
             """).lstrip()
 
         # notice only needs to be appended to the README for 7.13.x
