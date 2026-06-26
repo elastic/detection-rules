6165c4997eb671171f9918085c966ccb32d9fec1
eric-forte-elastic <eric.forte@elastic.co>
2026-06-26 13:42:36 -0400

    Fix formatting

--- detection_rules/rule_validators.py
+++ detection_rules/rule_validators.py
@@ -852,10 +852,14 @@ class ESQLValidator(QueryValidator):
                 misc.get_kibana_client(**resolved_kibana_options) as kibana_client,  # type: ignore[reportUnknownVariableType]
                 misc.get_elasticsearch_client(**resolved_elastic_options) as elastic_client,  # type: ignore[reportUnknownVariableType]
             ):
+                query = data.query
+                if query is None:
+                    raise ValueError("ES|QL remote validation requires a query.")
+
                 _ = self.remote_validate_rule(
                     kibana_client,
                     elastic_client,
-                    data.query,
+                    query,
                     rule_meta,
                     data.rule_id,
                 )
