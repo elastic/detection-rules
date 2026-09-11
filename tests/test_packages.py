e449cfe8255c55ef994cd79f51826370f59af789
Eric Forte <119343520+eric-forte-elastic@users.noreply.github.com>
2026-09-11 14:11:07 -0400

    Apply suggestion from @eric-forte-elastic

--- tests/test_packages.py
+++ tests/test_packages.py
@@ -31,7 +31,7 @@ from tests.base import BaseRuleTest
 package_configs = Package.load_configs()
 
 # 4 rule sample per type to catch cases where hashing might miss a rule contents change incorrectly.
-# This will help us catch logic errors that may be introduced over time in the hashing and API contents build.
+# This can catch logic errors that may be introduced over time in the hashing and API contents build.
 UNCACHED_HASH_SAMPLE_PER_KIND = 4
 
 
